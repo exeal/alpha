ceaf155ebb19abb8f3a4ab62081a2537e830a693
exeal <luigi.irmr@gmail.com>
2011-05-06 14:12:44 +0900

    Added kill-ring.* files.

--- ascension/ascension/text-editor/session.hpp
+++ ascension/ascension/text-editor/session.hpp
@@ -8,9 +8,12 @@
 #define ASCENSION_SESSION_HPP
 
 #include <ascension/config.hpp>				// ASCENSION_NO_MIGEMO
-#include <ascension/corelib/listeners.hpp>	// detail.Listeners
-#include <ascension/kernel/fileio.hpp>		// fileio.PathCharacter
+#include <ascension/corelib/string-piece.hpp>
+#ifndef ASCENSION_NO_MIGEMO
+#	include <ascension/kernel/fileio.hpp>	// fileio.PathCharacter
+#endif // !ASCENSION_NO_MIGEMO
 #include <list>
+#include <memory>
 #include <vector>
 
 namespace ascension {
@@ -26,52 +29,6 @@ namespace ascension {
 	}
 
 	namespace texteditor {
-
-		/**
-		 * Interface for objects which are interested in changes of the kill ring.
-		 * @see KillRing
-		 */
-		class KillRingListener {
-		private:
-			/// The content of the kill ring was changed.
-			virtual void killRingChanged() = 0;
-			friend class KillRing;
-		};
-
-		// documentation is session.cpp
-		class KillRing {
-		public:
-			// constructor
-			explicit KillRing(std::size_t maximumNumberOfKills = ASCENSION_DEFAULT_MAXIMUM_KILLS) /*throw()*/;
-			// listeners
-			void addListener(KillRingListener& listener);
-			void removeListener(KillRingListener& listener);
-/*			// kill
-			void copyRegion(const kernel::Document& document, const kernel::Region& region);
-			void killRegion(kernel::Document& document, const kernel::Region& region);
-			// yank
-			void yank(std::size_t index = 0);
-			void yankPop(std::size_t index = -1);
-*/			// low level accesses
-			void addNew(const String& text, bool rectangle, bool replace = false);
-			void append(const String& text, bool prepend);
-			const std::pair<String, bool>& get(std::ptrdiff_t places = 0) const;
-			const std::pair<String, bool>& setCurrent(std::ptrdiff_t places);
-			// number
-			std::size_t maximumNumberOfKills() const /*throw()*/;
-			std::size_t numberOfKills() const /*throw()*/;
-//			void setMaximumNumberOfKills(std::size_t capacity) /*throw()*/;
-		private:
-			typedef std::list<std::pair<String, bool> > Contents;
-			Contents::iterator at(ptrdiff_t index) const;
-			void interprogramCopy(const String& text, bool rectangle);
-			std::pair<String, bool> interprogramPaste();
-		private:
-			Contents contents_;	// plain-text vs. rectangle-flag
-			Contents::iterator yankPointer_;
-			const std::size_t maximumNumberOfKills_;
-			detail::Listeners<KillRingListener> listeners_;
-		};
 
 #ifdef ASCENSION_OS_WINDOWS
 		/**
@@ -110,6 +67,8 @@ namespace ascension {
 			HKL keyboardLayout_;
 		};
 #endif // ASCENSION_OS_WINDOWS
+
+		class KillRing;
 
 		/**
 		 * @note This class is not derivable.
@@ -164,7 +123,6 @@ namespace ascension {
 		}
 
 	} // namespace texteditor
-
 } // namespace ascension
 
 #endif // !ASCENSION_SESSION_HPP
---
+++ ascension/src/text-editor/kill-ring.cpp
@@ -0,0 +1,132 @@
+/**
+ * @file kill-ring.cpp
+ * @author exeal
+ * @date 2006-2011 was session.cpp
+ * @date 2011-05-06 separated from session.hpp
+ */
+
+#include <ascension/text-editor/kill-ring.hpp>
+#include <ascension/corelib/basic-exceptions.hpp>
+#include <algorithm>
+using namespace ascension;
+using namespace ascension::texteditor;
+using namespace std;
+
+
+// KillRing ///////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @class ascension::texteditor::KillRing
+ */
+
+/**
+ * Constructor.
+ * @param maximumNumberOfKills Initial maximum number of kills. This setting can be change by
+ *                             @c #setMaximumNumberOfKills later
+ */
+KillRing::KillRing(size_t maximumNumberOfKills /* = ASCENSION_DEFAULT_MAXIMUM_KILLS */) /*throw()*/
+		: yankPointer_(contents_.end()), maximumNumberOfKills_(maximumNumberOfKills) {
+}
+
+/**
+ * Registers the listener.
+ * @param listener The listener to be registered
+ * @throw std#invalid_argument @a listener is already registered
+ */
+void KillRing::addListener(KillRingListener& listener) {
+	listeners_.add(listener);
+}
+
+/**
+ * Makes the given content tha latest kill in the kill ring.
+ * @param text The content
+ * @param rectangle Set to @c true if the content is a rectangle
+ * @param replace Set to @c true to replace the front of the kill ring. Otherwise the new content
+ *                will be added
+ */
+void KillRing::addNew(const String& text, bool rectangle, bool replace /* = false */) {
+	if(!contents_.empty() && replace)
+		contents_.front() = make_pair(text, rectangle);
+	else {
+		contents_.push_front(make_pair(text, rectangle));
+		if(contents_.size() > maximumNumberOfKills_)
+			contents_.pop_back();
+	}
+	yankPointer_ = contents_.begin();
+	listeners_.notify(&KillRingListener::killRingChanged);
+}
+
+/**
+ *
+ * @param text
+ * @param prepend
+ */
+void KillRing::append(const String& text, bool prepend) {
+	if(contents_.empty())
+		return addNew(text, false, true);
+	else if(!prepend)
+		contents_.front().first.append(text);
+	else
+		contents_.front().first.insert(0, text);
+	yankPointer_ = contents_.begin();
+	listeners_.notify(&KillRingListener::killRingChanged);
+}
+
+KillRing::Contents::iterator KillRing::at(ptrdiff_t index) const {
+	if(contents_.empty())
+		throw IllegalStateException("the kill ring is empty.");
+	Contents::iterator i(const_cast<KillRing*>(this)->yankPointer_);
+	if(index >= 0) {
+		for(index -= index - (index % contents_.size()); index > 0; --index) {
+			if(++i == contents_.end())
+				i = const_cast<KillRing*>(this)->contents_.begin();
+		}
+	} else {
+		for(index += -index -(-index % contents_.size()); index < 0; ++index) {
+			if(i == contents_.begin())
+				i = const_cast<KillRing*>(this)->contents_.end();
+			--i;
+		}
+	}
+	return i;
+}
+
+/**
+ * Returns the content.
+ * @param places
+ * @return the content
+ * @throw IllegalStateException The kill ring is empty
+ */
+const pair<String, bool>& KillRing::get(ptrdiff_t places /* = 0 */) const {
+	return *at(places);
+}
+
+/// Returns the maximum number of kills.
+size_t KillRing::maximumNumberOfKills() const /*throw()*/ {
+	return maximumNumberOfKills_;
+}
+
+/// Returns the number of kills.
+size_t KillRing::numberOfKills() const /*throw()*/ {
+	return contents_.size();
+}
+
+/**
+ * Removes the listener.
+ * @param listener The listener to be removed
+ * @throw std#invalid_argument @a listener is not registered
+ */
+void KillRing::removeListener(KillRingListener& listener) {
+	listeners_.remove(listener);
+}
+
+/**
+ * Rotates the yanking point by the given number of places.
+ * @param places
+ * @return The content
+ * @throw IllegalStateException The kill ring is empty
+ */
+const pair<String, bool>& KillRing::setCurrent(ptrdiff_t places) {
+	yankPointer_ = at(places);
+	return *yankPointer_;
+}
