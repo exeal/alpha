696e203eb9e0532e7b6d137964739d5ce92fe7d9
exeal <luigi.irmr@gmail.com>
2011-10-05 00:01:38 +0900

    Added viewer/default-mouse-input-strategy.* files.

---
+++ ascension/ascension/viewer/default-mouse-input-strategy.hpp
@@ -0,0 +1,136 @@
+/**
+ * @file viewer.hpp
+ * This header defines several visual presentation classes.
+ * @author exeal
+ * @date 2003-2006 was EditView.h
+ * @date 2006-2011 was viewer.hpp
+ * @date 2011-10-04 separated from viewer.hpp
+ */
+
+#ifndef ASCENSION_DEFAULT_MOUSE_INPUT_STRATEGY_HPP
+#define ASCENSION_DEFAULT_MOUSE_INPUT_STRATEGY_HPP
+
+#include <ascension/config.hpp>	// ASCENSION_DEFAULT_TEXT_READING_DIRECTION, ...
+#include <ascension/platforms.hpp>
+#include <ascension/corelib/timer.hpp>	// Timer
+#include <ascension/kernel/position.hpp>	// kernel.Position
+#include <ascension/viewer/caret-observers.hpp>
+#include <ascension/viewer/base/widget.hpp>
+#include <memory>	// std.auto_ptr
+#include <utility>	// std.pair
+#if defined(ASCENSION_WINDOW_SYSTEM_GTK)
+#elif defined(ASCENSION_WINDOW_SYSTEM_QUARTZ)
+#elif defined(ASCENSION_WINDOW_SYSTEM_QT)
+#elif defined(ASCENSION_WINDOW_SYSTEM_WIN32)
+#	include <ascension/win32/com/unknown-impl.hpp>
+#	include <shlobj.h>	// IDragSourceHelper, IDropTargetHelper
+#endif
+
+
+namespace ascension {
+
+	namespace presentation {
+		namespace hyperlink {
+			class Hyperlink;
+		}
+	}
+
+	namespace viewers {
+
+		// the documentation is user-input.cpp
+		class DefaultMouseInputStrategy : public MouseInputStrategy,
+#ifdef ASCENSION_WINDOW_SYSTEM_WIN32
+				win32::com::IUnknownImpl<
+					typelist::Cat<
+						ASCENSION_WIN32_COM_INTERFACE_SIGNATURE(IDropSource), typelist::Cat<
+							ASCENSION_WIN32_COM_INTERFACE_SIGNATURE(IDropTarget)
+						>
+					>, win32::com::NoReferenceCounting
+				>,
+#endif // ASCENSION_WINDOW_SYSTEM_WIN32
+				private HasTimer {
+			ASCENSION_UNASSIGNABLE_TAG(DefaultMouseInputStrategy);
+		public:
+			/**
+			 * Defines drag-and-drop support levels.
+			 * @see DefaultMouseInputStrategy#DefaultMouseInputStrategy
+			 */
+			enum DragAndDropSupport {
+				/// Disables drag-and-drop.
+				DONT_SUPPORT_DND,
+				/// Enables drag-and-drop.
+				SUPPORT_DND,
+				/// Enables drag-and-drop and shows a drag-image.
+				SUPPORT_DND_WITH_DRAG_IMAGE,
+				/// Enables drag-and-drop and shows a selection-highlighted drag-image.
+				SUPPORT_DND_WITH_SELECTED_DRAG_IMAGE
+			};
+		public:
+			explicit DefaultMouseInputStrategy(
+				DragAndDropSupport dragAndDropSupportLevel = SUPPORT_DND_WITH_SELECTED_DRAG_IMAGE);
+		private:
+			virtual bool handleLeftButtonDoubleClick(const graphics::NativePoint& position, int modifiers);
+			virtual bool handleRightButton(Action action, const graphics::NativePoint& position, int modifiers);
+			virtual bool handleX1Button(Action action, const graphics::NativePoint& position, int modifiers);
+			virtual bool handleX2Button(Action action, const graphics::NativePoint& position, int modifiers);
+		private:
+			static graphics::NativeSize calculateDnDScrollOffset(const TextViewer& viewer);
+#ifdef ASCENSION_WINDOW_SYSTEM_WIN32
+			HRESULT doDragAndDrop();
+#endif // ASCENSION_WINDOW_SYSTEM_WIN32
+			bool endAutoScroll();
+			void extendSelection(const kernel::Position* to = 0);
+			void handleLeftButtonPressed(const graphics::NativePoint& position, int modifiers);
+			void handleLeftButtonReleased(const graphics::NativePoint& position, int modifiers);
+			// MouseInputStrategy
+			void captureChanged();
+			void install(TextViewer& viewer);
+			void interruptMouseReaction(bool forKeyboardInput);
+			bool mouseButtonInput(Action action, const base::MouseButtonInput& input);
+			void mouseMoved(const base::LocatedUserInput& input);
+			void mouseWheelRotated(const base::MouseWheelInput& input);
+			bool showCursor(const graphics::NativePoint& position);
+			void uninstall();
+			// HasTimer
+			void timeElapsed(Timer& timer);
+#ifdef ASCENSION_WINDOW_SYSTEM_WIN32
+			// IDropSource
+			STDMETHODIMP QueryContinueDrag(BOOL escapePressed, DWORD keyState);
+			STDMETHODIMP GiveFeedback(DWORD effect);
+			// IDropTarget
+			STDMETHODIMP DragEnter(IDataObject* data, DWORD keyState, POINTL pt, DWORD* effect);
+			STDMETHODIMP DragOver(DWORD keyState, POINTL pt, DWORD* effect);
+			STDMETHODIMP DragLeave();
+			STDMETHODIMP Drop(IDataObject* data, DWORD keyState, POINTL pt, DWORD* effect);
+#endif // ASCENSION_WINDOW_SYSTEM_WIN32
+		private:
+			TextViewer* viewer_;
+			enum {
+				NONE = 0x00,
+				SELECTION_EXTENDING_MASK = 0x10, EXTENDING_CHARACTER_SELECTION, EXTENDING_WORD_SELECTION, EXTENDING_LINE_SELECTION,
+				AUTO_SCROLL_MASK = 0x20, APPROACHING_AUTO_SCROLL, AUTO_SCROLL_DRAGGING, AUTO_SCROLL,
+				DND_MASK = 0x40, APPROACHING_DND, DND_SOURCE, DND_TARGET
+			} state_;
+			graphics::NativePoint dragApproachedPosition_;	// in client coordinates
+			struct Selection {
+				length_t initialLine;	// line of the anchor when entered the selection extending
+				std::pair<length_t, length_t> initialWordColumns;
+			} selection_;
+			struct DragAndDrop {
+				DragAndDropSupport supportLevel;
+				length_t numberOfRectangleLines;
+#ifdef ASCENSION_WINDOW_SYSTEM_WIN32
+				win32::com::ComPtr<IDragSourceHelper> dragSourceHelper;
+				win32::com::ComPtr<IDropTargetHelper> dropTargetHelper;
+#endif // ASCENSION_WINDOW_SYSTEM_WIN32
+			} dnd_;
+			std::auto_ptr<base::Widget> autoScrollOriginMark_;
+			const presentation::hyperlink::Hyperlink* lastHoveredHyperlink_;
+			Timer timer_;
+			static const unsigned int SELECTION_EXPANSION_INTERVAL, DRAGGING_TRACK_INTERVAL;
+		};
+	}
+
+} // namespace ascension.viewers
+
+#endif // !ASCENSION_DEFAULT_MOUSE_INPUT_STRATEGY_HPP
--- ascension/ascension/viewer/viewer-observers.hpp
+++ ascension/ascension/viewer/viewer-observers.hpp
@@ -6,6 +6,11 @@
 
 #ifndef VIEWER_OBSERVERS_HPP
 #define VIEWER_OBSERVERS_HPP
+#include <ascension/graphics/geometry.hpp>	// graphics.NativeSize
+#include <ascension/platforms.hpp>
+#if defined(ASCENSION_WINDOW_SYSTEM_WIN32)
+#	include <ascension/win32/windows.hpp>
+#endif
 
 namespace ascension {
 	namespace viewers {
@@ -37,6 +42,83 @@ namespace ascension {
 		private:
 			/// The size of the viewer was changed.
 			virtual void viewerDisplaySizeChanged() = 0;
+			friend class TextViewer;
+		};
+#if 0
+		/**
+		 * Interface of objects which define how the text editors react to the users' keyboard
+		 * input. Ascension also provides the standard implementation of this interface
+		 * @c DefaultKeyboardInputStrategy.
+		 * @see TextViewer#set
+		 */
+		class KeyboardInputStrategy {
+		};
+#endif
+		namespace base {
+			class LocatedUserInput;
+			class MouseButtonInput;
+			class MouseWheelInput;
+		}
+
+		/**
+		 * Interface of objects which define how the text editors react to the users' mouse input.
+		 * @note An instance of @c IMouseInputStrategy can't be shared multiple text viewers.
+		 * @see TextViewer#setMouseInputStrategy
+		 */
+		class MouseInputStrategy {
+		public:
+			/// Destructor.
+			virtual ~MouseInputStrategy() /*throw()*/ {}
+		protected:
+			/// Actions of the mouse input.
+			enum Action {
+				PRESSED,		///< The button was pressed (down).
+				RELEASED,		///< The button was released (up).
+				DOUBLE_CLICKED	///< The button was double-clicked.
+			};
+		private:
+			/// The viewer lost the mouse capture.
+			virtual void captureChanged() = 0;
+			/**
+			 * Installs the strategy.
+			 * @param viewer The text viewer uses the strategy. The window had been created at this
+			 *               time
+			 */
+			virtual void install(TextViewer& viewer) = 0;
+			/**
+			 * Interrupts the progressive mouse reaction.
+			 * This method must be called before @c #uninstall call.
+			 * @param forKeyboardInput @c true if the mouse reaction should interrupt because the
+			 *                         keyboard input was occurred
+			 */
+			virtual void interruptMouseReaction(bool forKeyboardInput) = 0;
+			/**
+			 * The mouse input was occurred and the viewer had focus.
+			 * @param action The action of the input
+			 * @param input The input information
+			 * @return @c true if the strategy processed
+			 */
+			virtual bool mouseButtonInput(Action action, const base::MouseButtonInput& input) = 0;
+			/**
+			 * The mouse was moved and the viewer had focus.
+			 * @param input The input information
+			 * @return @c true if the strategy processed
+			 */
+			virtual void mouseMoved(const base::LocatedUserInput& input) = 0;
+			/**
+			 * The mouse wheel was rolated and the viewer had focus.
+			 * @param input The input information
+			 */
+			virtual void mouseWheelRotated(const base::MouseWheelInput& input) = 0;
+			/**
+			 * Shows a cursor on the viewer.
+			 * @param position The cursor position (client coordinates)
+			 * @retval @c true if the callee showed a cursor
+			 * @retval @c false if the callee did not know the appropriate cursor
+			 */
+			virtual bool showCursor(const graphics::NativePoint& position) = 0;
+			/// Uninstalls the strategy. The window is not destroyed yet at this time.
+			virtual void uninstall() = 0;
 			friend class TextViewer;
 		};
 
