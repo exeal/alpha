73a9c6e2835d72daa84e3443f0be746602ee0aa3
exeal <exeal@users.sourceforge.jp>
2012-05-05 01:07:00 +0900

    Added ascension/win32/com/smart-pointer.hpp defines SmartPointer class template.

--- ascension/ascension/win32/com/dispatch-impl.hpp
+++ ascension/ascension/win32/com/dispatch-impl.hpp
@@ -6,153 +6,164 @@
 
 #ifndef ASCENSION_WIN32_DISPATCH_IMPL_HPP
 #define ASCENSION_WIN32_DISPATCH_IMPL_HPP
-#include "com.hpp"
+#include <ascension/win32/com/com.hpp>
+#include <ascension/win32/com/smart-pointer.hpp>
+#include <OAIdl.h>	// IDispatch, ITypeLib
+#include <OCIdl.h>	// IProvideClassInfo2
 
 namespace ascension {
 	namespace win32 {
 		namespace com {
-			namespace ole {
 
-// IProvideClassInfo2Impl ///////////////////////////////////////////////////
-	
+			// IProvideClassInfo2Impl /////////////////////////////////////////////////////////////
+
 #ifdef __IProvideClassInfo2_INTERFACE_DEFINED__
 
-/**
- * Standard implementation of @c IProvideClassInfo2 interface.
- * @tparam clsid The CLSID
- * @tparam iid The IID
- * @tparam libid The LIBID
- * @tparam majorVersion The major version
- * @tparam minorVersion The minor version
- */
-template<const CLSID* clsid, const IID* iid, const GUID* libid, WORD majorVersion = 1, WORD minorVersion = 0>
-class IProvideClassInfo2Impl : /*virtual*/ public IProvideClassInfo2 {
-public:
-	/// Constructor.
-	IProvideClassInfo2Impl() {
-		ComPtr<ITypeLib> typeLib;
-		HRESULT hr = ::LoadRegTypeLib(*libid, majorVersion, minorVersion, 0, typeLib.initialize());
-		assert(SUCCEEDED(hr));
-		typeLib->GetTypeInfoOfGuid(*iid, typeInfo_.initialize());
-		assert(SUCCEEDED(hr));
-	}
-	///	@see IProvideClassInfo#GetClassInfo
-	STDMETHOD(GetClassInfo)(ITypeInfo** ppTypeInfo) {
-		MANAH_VERIFY_POINTER(ppTypeInfo);
-		(*ppTypeInfo = typeInfo_.get())->AddRef();
-		return S_OK;
-	}
-	///	@see IProvideClassInfo2#GetGUID
-	STDMETHOD(GetGUID)(DWORD dwGuidKind, GUID* pGUID) {
-		MANAH_VERIFY_POINTER(pGUID);
-		if(dwGuidKind == GUIDKIND_DEFAULT_SOURCE_DISP_IID && clsid != 0) {
-			*pGUID = iid;
-			return S_OK;
-		}
-		*pGUID = 0;
-		return E_INVALIDARG;
-	}
-private:
-	ComPtr<ITypeInfo> typeInfo_;
-};
+			/**
+			 * Standard implementation of @c IProvideClassInfo2 interface.
+			 * @tparam clsid The CLSID
+			 * @tparam iid The IID
+			 * @tparam libid The LIBID
+			 * @tparam majorVersion The major version
+			 * @tparam minorVersion The minor version
+			 */
+			template<const CLSID* clsid, const IID* iid,
+				const GUID* libid, WORD majorVersion = 1, WORD minorVersion = 0>
+			class IProvideClassInfo2Impl : /*virtual*/ public IProvideClassInfo2 {
+			public:
+				/// Constructor.
+				IProvideClassInfo2Impl() {
+					SmartPointer<ITypeLib> typeLib;
+					HRESULT hr = ::LoadRegTypeLib(*libid, majorVersion, minorVersion, 0, typeLib.initialize());
+					assert(SUCCEEDED(hr));
+					typeLib->GetTypeInfoOfGuid(*iid, typeInfo_.initialize());
+					assert(SUCCEEDED(hr));
+				}
+				///	@see IProvideClassInfo#GetClassInfo
+				STDMETHOD(GetClassInfo)(ITypeInfo** ppTypeInfo) {
+					ASCENSION_WIN32_VERIFY_COM_POINTER(ppTypeInfo);
+					(*ppTypeInfo = typeInfo_.get())->AddRef();
+					return S_OK;
+				}
+				///	@see IProvideClassInfo2#GetGUID
+				STDMETHOD(GetGUID)(DWORD dwGuidKind, GUID* pGUID) {
+					ASCENSION_WIN32_VERIFY_COM_POINTER(pGUID);
+					if(dwGuidKind == GUIDKIND_DEFAULT_SOURCE_DISP_IID && clsid != 0) {
+						*pGUID = iid;
+						return S_OK;
+					}
+					*pGUID = 0;
+					return E_INVALIDARG;
+				}
+			private:
+				SmartPointer<ITypeInfo> typeInfo_;
+			};
 
 #endif // __IProvideClassInfo2_INTERFACE_DEFINED__
 
 
-/**
- * Loads the type library from LIBID.
- * @tparam libid The LIBID of the type library
- * @tparam iid The IID of the type to load
- * @tparam majorVersion The major version
- * @tparam minorVersion The minor version
- */
-template<const GUID* libid, const IID* iid, WORD majorVersion = 1, WORD minorVersion = 0> class TypeInformationFromRegistry {
-public:
-	TypeInformationFromRegistry() {
-		ComPtr<ITypeLib> typeLibrary;
-		assert(libid != nullptr && iid != nullptr);
-		HRESULT hr = ::LoadRegTypeLib(*libid, majorVersion, minorVersion, 0, typeLibrary.initialize());
-		assert(SUCCEEDED(hr));
-		hr = typeLibrary->GetTypeInfoOfGuid(*iid, typeInformation_.initialize());
-		assert(SUCCEEDED(hr));
-	}
-	ComPtr<ITypeInfo> get() const /*throw()*/ {return typeInformation_;}
-private:
-	ComPtr<ITypeInfo> typeInformation_;
-};
+			/**
+			 * Loads the type library from LIBID.
+			 * @tparam libid The LIBID of the type library
+			 * @tparam iid The IID of the type to load
+			 * @tparam majorVersion The major version
+			 * @tparam minorVersion The minor version
+			 */
+			template<const GUID* libid, const IID* iid, WORD majorVersion = 1, WORD minorVersion = 0>
+			class TypeInformationFromRegistry {
+			public:
+				TypeInformationFromRegistry() {
+					SmartPointer<ITypeLib> typeLibrary;
+					assert(libid != nullptr && iid != nullptr);
+					HRESULT hr = ::LoadRegTypeLib(*libid, majorVersion, minorVersion, 0, typeLibrary.initialize());
+					assert(SUCCEEDED(hr));
+					hr = typeLibrary->GetTypeInfoOfGuid(*iid, typeInformation_.initialize());
+					assert(SUCCEEDED(hr));
+				}
+				SmartPointer<ITypeInfo> get() const /*throw()*/ {return typeInformation_;}
+			private:
+				SmartPointer<ITypeInfo> typeInformation_;
+			};
 
-/**
- * Loads the type library from file.
- * @tparam TypeLibPath The class provides the path name of the type library.
- * @tparam iid The IID of the type to load
- */
-template<typename TypeLibraryPath, const IID* iid> class TypeInformationFromPath {
-public:
-	TypeInformationFromPath() {
-		ComPtr<ITypeLib> typeLibrary;
-		HRESULT hr = ::LoadTypeLib(TypeLibraryPath::get(), typeLibrary.initialize());
-		assert(SUCCEEDED(hr));
-		hr = typeLibrary->GetTypeInfoOfGuid(*iid, typeInformation_.initialize());
-		assert(SUCCEEDED(hr));
-	}
-	/// Returns an @c ITypeInfo instance.
-	ComPtr<ITypeInfo> get() const /*throw()*/ {return typeInformation_;}
-private:
-	ComPtr<ITypeInfo> typeInformation_;
-};
+			/**
+			 * Loads the type library from file.
+			 * @tparam TypeLibPath The class provides the path name of the type library.
+			 * @tparam iid The IID of the type to load
+			 */
+			template<typename TypeLibraryPath, const IID* iid>
+			class TypeInformationFromPath {
+			public:
+				TypeInformationFromPath() {
+					SmartPointer<ITypeLib> typeLibrary;
+					HRESULT hr = ::LoadTypeLib(TypeLibraryPath::get(), typeLibrary.initialize());
+					assert(SUCCEEDED(hr));
+					hr = typeLibrary->GetTypeInfoOfGuid(*iid, typeInformation_.initialize());
+					assert(SUCCEEDED(hr));
+				}
+				/// Returns an @c ITypeInfo instance.
+				SmartPointer<ITypeInfo> get() const /*throw()*/ {return typeInformation_;}
+			private:
+				SmartPointer<ITypeInfo> typeInformation_;
+			};
+			
+			/**
+			 * Loads the type library from program module.
+			 * @tparam iid The IID of the type to load
+			 */
+			template<const IID* iid>
+			class TypeInformationFromExecutable {
+			public:
+				TypeInformationFromExecutable() {
+					WCHAR programName[MAX_PATH];
+					const DWORD n = ::GetModuleFileNameW(nullptr, programName, ASCENSION_COUNTOF(programName));
+					if(n != 0 && n != ASCENSION_COUNTOF(programName)) {
+						SmartPointer<ITypeLib> typeLibrary;
+						if(SUCCEEDED(::LoadTypeLib(programName, typeLibrary.initialize())))
+							typeLibrary->GetTypeInfoOfGuid(*iid, typeInformation_.initialize());
+					}
+				}
+				SmartPointer<ITypeInfo> get() const /*throw()*/ {return typeInformation_;}
+			private:
+				SmartPointer<ITypeInfo> typeInformation_;
+			};
 
-/**
- * Loads the type library from program module.
- * @param iid The IID of the type to load
- */
-template<const IID* iid> class TypeInformationFromExecutable {
-public:
-	TypeInformationFromExecutable() {
-		WCHAR programName[MAX_PATH];
-		const DWORD n = ::GetModuleFileNameW(nullptr, programName, MANAH_COUNTOF(programName));
-		if(n != 0 && n != MANAH_COUNTOF(programName)) {
-			ComPtr<ITypeLib> typeLibrary;
-			if(SUCCEEDED(::LoadTypeLib(programName, typeLibrary.initialize())))
-				typeLibrary->GetTypeInfoOfGuid(*iid, typeInformation_.initialize());
+
+			// IDispatchImpl //////////////////////////////////////////////////////////////////////////////////
+
+			/**
+			 * Standard implementation of @c IDispatch interface.
+			 * @tparam Base The base class derived from dispatch interfaces to implement
+			 * @tparam TypeInformationProvider Provides @c ITypeInfo
+			 */
+			template<typename Base, typename TypeInformationProvider>
+			class IDispatchImpl : public Base {
+			public:
+				/// @see IDispatch#GetIDsOfNames
+				STDMETHODIMP GetIDsOfNames(REFIID iid, OLECHAR** names, unsigned int numberOfNames, LCID, DISPID* id) {
+					return (iid == IID_NULL) ? ::DispGetIDsOfNames(tip_.get().get(), names, numberOfNames, id) : E_INVALIDARG;
+				}
+				/// @see IDispatch#GetTypeInfo
+				STDMETHODIMP GetTypeInfo(unsigned int index, LCID, ITypeInfo** typeInformation) {
+					ASCENSION_WIN32_VERIFY_COM_POINTER(typeInformation);
+					return (index == 0) ? ((*typeInformation = tip_.get().get())->AddRef(), S_OK) : DISP_E_BADINDEX;
+				}
+				/// @see IDispatch#GetTypeInfoCount
+				STDMETHODIMP GetTypeInfoCount(unsigned int* number) {
+					ASCENSION_WIN32_VERIFY_COM_POINTER(number);
+					return (*number = 1), S_OK;
+				}
+				/// @see IDispatch#Invoke
+				STDMETHODIMP Invoke(DISPID id, REFIID iid, LCID, WORD flags,
+						DISPPARAMS* parameters, VARIANT* result, EXCEPINFO* exception, unsigned int* argErr) {
+					return (iid == IID_NULL) ? ::DispInvoke(static_cast<IDispatch*>(this),
+						tip_.get().get(), id, flags, parameters, result, exception, argErr) : DISP_E_UNKNOWNINTERFACE;
+				}
+			private:
+				TypeInformationProvider tip_;
+			};
+
 		}
 	}
-	ComPtr<ITypeInfo> get() const /*throw()*/ {return typeInformation_;}
-private:
-	ComPtr<ITypeInfo> typeInformation_;
-};
-
-
-// IDispatchImpl class definition and implementation
-/////////////////////////////////////////////////////////////////////////////
-
-/**
- * Standard implementation of @c IDispatch interface.
- * @tparam Base The base class derived from dispatch interfaces to implement
- * @tparam TypeInformationProvider Provides @c ITypeInfo
- */
-template<typename Base, typename TypeInformationProvider>
-class IDispatchImpl : public Base {
-public:
-	/// @see IDispatch#GetIDsOfNames
-	STDMETHODIMP GetIDsOfNames(REFIID iid, OLECHAR** names, unsigned int numberOfNames, LCID, DISPID* id) {
-		return (iid == IID_NULL) ? ::DispGetIDsOfNames(tip_.get().get(), names, numberOfNames, id) : E_INVALIDARG;}
-	/// @see IDispatch#GetTypeInfo
-	STDMETHODIMP GetTypeInfo(unsigned int index, LCID, ITypeInfo** typeInformation) {
-		MANAH_VERIFY_POINTER(typeInformation);
-		return (index == 0) ? ((*typeInformation = tip_.get().get())->AddRef(), S_OK) : DISP_E_BADINDEX;}
-	/// @see IDispatch#GetTypeInfoCount
-	STDMETHODIMP GetTypeInfoCount(unsigned int* number) {MANAH_VERIFY_POINTER(number); *number = 1; return S_OK;}
-	/// @see IDispatch#Invoke
-	STDMETHODIMP Invoke(DISPID id, REFIID iid, LCID, WORD flags, DISPPARAMS* parameters, VARIANT* result,
-		EXCEPINFO* exception, unsigned int* argErr) {return (iid == IID_NULL) ? ::DispInvoke(static_cast<IDispatch*>(this),
-		tip_.get().get(), id, flags, parameters, result, exception, argErr) : DISP_E_UNKNOWNINTERFACE;}
-private:
-	TypeInformationProvider tip_;
-};
-
-			}
-		}
-	}
-} // namespace ascension.win32.com.ole
+} // namespace ascension.win32.com
 
 #endif // !ASCENSION_WIN32_DISPATCH_IMPL_HPP
---
+++ ascension/ascension/win32/com/smart-pointer.hpp
@@ -0,0 +1,191 @@
+/**
+ * @file smart-pointer.hpp
+ * @author exeal
+ * @date 2002-2009 was manah/com/common.hpp
+ * @date 2010-2012 was com.hpp
+ * @date 2012-05-04 separated from com.hpp
+ */
+
+#ifndef ASCENSION_WIN32_SMART_POINTER_HPP
+#define ASCENSION_WIN32_SMART_POINTER_HPP
+
+#include <objbase.h>	// CoCreateInstance
+#include <cassert>
+#include <utility>		// std.swap
+#include <boost/operators.hpp>
+
+
+namespace ascension {
+	namespace win32 {
+		namespace com {
+			/// A proxy returned by @c SmartPointer#operator->.
+			template<typename T> class SmartPointerProxy : public T {
+			private:
+				STDMETHOD_(ULONG, AddRef)() = 0;
+				STDMETHOD_(ULONG, Release)() = 0;
+				T** operator &() const /*throw()*/;	// prohibits &*p
+			};
+
+			/**
+			 * COM smart pointer.
+			 * @tparam T The interface type
+			 * @tparam iid The GUID of @a T
+			 */
+			template<typename T, const IID* iid = &__uuidof(T)>
+			class SmartPointer :
+				public boost::totally_ordered<T*, SmartPointer<T, iid>>,
+				public boost::totally_ordered<SmartPointer<T, iid>, SmartPointer<T, iid>> {
+			public:
+				typedef T element_type;	///< The interface type.
+			public:
+				/// Default constructor.
+				SmartPointer() /*noexcept*/ : pointee_(nullptr) {}
+				/// Null pointer constructor.
+				SmartPointer(std::nullptr_t) /*noexcept*/ : pointee_(nullptr) {}
+				/// Constructor with an interface pointer.
+				template<typename U>
+				explicit SmartPointer(U* p) /*noexcept*/ : pointee_(nullptr) {
+					if(p != nullptr)
+						p->QueryInterface(iid, initializePPV());
+				}
+				/// Constructor with an interface pointer.
+				template<>
+				explicit SmartPointer<element_type, iid>(element_type* p) /*noexcept*/ : pointee_(p) {
+					if(pointee_ != nullptr)
+						pointee_->AddRef();
+				}
+				/// Copy-constructor.
+				SmartPointer(const SmartPointer<element_type, iid>& other) /*noexcept*/ : pointee_(other.pointee_) {
+					if(pointee_ != nullptr)
+						pointee_->AddRef();
+				}
+				/// Copy-constructor.
+				template<typename U, const IID* riid>
+				SmartPointer(const SmartPointer<U, riid>& other) /*noexcept*/ : pointee_(nullptr) {
+					if(other.pointee_ != nullptr)
+						other.pointee_->QueryInterface(iid, initializePPV());
+				}
+				/// Move-constructor.
+				SmartPointer(SmartPointer<element_type, iid>&& other) /*noexcept*/ : pointee_(other.pointee_) {
+					if(pointee_ != nullptr) {
+						pointee_->AddRef();
+						other.reset();
+					}
+				}
+				/// Move-constructor.
+				template<typename U, const IID* riid>
+				SmartPointer(SmartPointer<U, riid>&& other) /*noexcept*/ : pointee_(nullptr) {
+					if(pointee_ != nullptr) {
+						if(SUCCEEDED(other.pointee_->QueryInterface(iid, initializePPV())))
+							other.reset();
+					}
+				}
+				/// Copy-assignment operator.
+				template<typename U, const IID* riid>
+				SmartPointer<element_type, iid>& operator=(const SmartPointer<U, riid>& other) /*noexcept*/ {
+					SmartPointer<element_type, iid>(other).swap(*this);
+					return *this;
+				}
+				/// Move-assignment operator.
+				template<typename U, const IID* riid>
+				SmartPointer<element_type, iid>& operator=(SmartPointer<U, riid>&& other) /*noexcept*/ {
+					SmartPointer<element_type, iid>(other).swap(*this);
+					return *this;
+				}
+				/// Destructor.
+				~SmartPointer() /*noexcept*/ {
+					if(pointee_ != nullptr)
+						pointee_->Release();
+				}
+				/// 
+				operator bool() const /*noexcept*/ {return get() != nullptr;}
+				/// Constructor creates instance by using @c CoCreateInstance.
+				static SmartPointer<element_type, iid> create(REFCLSID clsid, REFIID iid /* = __uuidof(Interface) */,
+						DWORD context = CLSCTX_ALL, IUnknown* outer = nullptr, HRESULT* hr = nullptr) {
+					SmartPointer<element_type, iid> p;
+					const HRESULT r = ::CoCreateInstance(clsid, outer, context, iid, p.initializePPV());
+					if(hr != 0)
+						*hr = r;
+					return p;
+				}
+				/// Returns the interface pointer.
+				SmartPointerProxy<element_type>* get() const /*noexcept*/ {
+					return static_cast<SmartPointerProxy<element_type>*>(pointee_);
+				}
+				/// Returns the output pointer for initialization.
+				element_type** initialize() /*noexcept*/ {
+					reset();
+					return &pointee_;
+				}
+				/// Returns the output pointer for initialization with type @c void**.
+				void** initializePPV() /*noexcept*/ {
+					return reinterpret_cast<void**>(initialize());
+				}
+				/// Returns true if the pointer addresses the same object.
+				bool equals(IUnknown* p) const /*noexcept*/ {
+					if(pointee_ == nullptr && p == nullptr)
+						return true;
+					else if(pointee_ == nullptr || p == nullptr)
+						return false;
+					SmartPointerProxy<IUnknown> ps[2];
+					pointee_->QueryInterface(IID_IUnknown, ps[0].initialize());
+					p->QueryInterface(IID_IUnknown, ps[1].initialize());
+					return ps[1].get() == ps[2].get();
+				}
+				/// Resets the pointer.
+				void reset() /*noexcept*/ {
+					SmartPointer<element_type, iid>().swap(*this);
+				}
+				/// Resets the pointer.
+				template<typename U>
+				void reset(U* p) /*noexcept*/ {
+					if(p != pointee_)
+						SmartPointer<element_type, iid>(p).swap(*this);
+				}
+				/// Swaps the two objects.
+				void swap(SmartPointerProxy<element_type>& other) /*noexcept*/ {
+					std::swap(pointee_, other.pointee_);
+				}
+				/// Member-access operator.
+				SmartPointerProxy<element_type>* operator->() const /*noexcept*/ {
+					assert(get() != nullptr);
+					return get();
+				}
+				/// Dereference operator.
+				SmartPointerProxy<element_type>& operator*() const /*noexcept*/ {
+					assert(get() != nullptr);
+					return *get();
+				}
+				/// Equality operator.
+				friend bool operator==(const element_type* lhs, const SmartPointer<element_type, iid>& rhs) /*noexcept*/ {
+					return operator==(rhs, lhs);
+				}
+				/// Equality operator.
+				friend bool operator==(const SmartPointer<element_type, iid>& lhs, const SmartPointer<element_type, iid>& rhs) /*noexcept*/ {
+					return lhs.pointee_ == rhs.pointee_;
+				}
+				/// Less than operator.
+				friend bool operator<(const element_type* lhs, const SmartPointer<element_type, iid>& rhs) /*noexcept*/ {
+					return lhs < rhs.pointee_;
+				}
+				/// Less than operator.
+				friend bool operator<(const SmartPointer<element_type, iid> lhs, const SmartPointer<element_type, iid>& rhs) /*noexcept*/ {
+					return lhs.pointee_ < rhs.pointee_;
+				}
+				/// Greater than operator.
+				friend bool operator>(const element_type* lhs, const SmartPointer<element_type, iid>& rhs) /*noexcept*/ {
+					return lhs > rhs.pointee_;
+				}
+				/// Greater than operator.
+				friend bool operator>(const SmartPointer<element_type, iid>& lhs, const SmartPointer<element_type, iid>& rhs) /*noexcept*/ {
+					return lhs.pointee_ > rhs.pointee_;
+				}
+			private:
+				SmartPointerProxy<element_type>* operator&() const;
+				element_type* pointee_;
+			};
+		}
+	}
+} // namespace ascension.win32.com
+
+#endif // !ASCENSION_WIN32_SMART_POINTER_HPP
