f0820d1628853ae68f2212f296f6136fb661d5cd
exeal <luigi.irmr@gmail.com>
2012-08-21 23:48:31 +0900

    Added font-description.hpp and font-family.hpp header files.

---
+++ ascension/ascension/graphics/font-description.hpp
@@ -0,0 +1,272 @@
+/**
+ * @file font-description.hpp
+ * @author exeal
+ * @date 2010-11-06 created
+ * @date 2010-2012 was font.hpp
+ * @date 2012-08-19 separated from font.hpp
+ */
+
+#ifndef ASCENSION_FONT_DESCRIPTION_HPP
+#define ASCENSION_FONT_DESCRIPTION_HPP
+
+#include <ascension/graphics/font-family.hpp>
+
+namespace ascension {
+	namespace graphics {
+		namespace font {
+
+			/// TrueType/OpenType font tag.
+			typedef uint32_t OpenTypeFontTag;
+
+			template<uint8_t c1, uint8_t c2 = ' ', uint8_t c3 = ' ', uint8_t c4 = ' '>
+			struct MakeOpenTypeFontTag {
+				static const OpenTypeFontTag value = (c1 << 24) | (c2 << 16) | (c3 << 8) | c4;
+			};
+
+			/**
+			 * Returns an 32-bit integer represents the given TrueType tag.
+			 * @tparam Character The character type of @a name
+			 * @param name The TrueType tag name
+			 * @param validate Set @c true to validate characters in @a name
+			 * @return The 32-bit integral TrueType tag value
+			 * @throw std#length_error The length of @a name is zero or greater four
+			 * @throw std#invalid_argument @a validate is @c true and any character in @a name was
+			 *                             invalid
+			 */
+			template<typename Character>
+			inline OpenTypeFontTag makeOpenTypeFontTag(const Character name[], bool validate = true) {
+				const std::size_t len = std::char_traits<Character>::length(name);
+				if(len == 0 || len > 4)
+					throw std::length_error("name");
+				OpenTypeFontTag tag = 0;
+				std::size_t i = 0;
+				for(; i < len; ++i) {
+					if(validate && (name[i] < 32 || name[i] > 126))
+						throw std::invalid_argument("name");
+					tag |= name[i] << ((3 - i) * 8);
+				}
+				for(; i < 4; ++i)
+					tag |= ' ' << ((3 - i) * 8);
+				return tag;
+			}
+
+			template<typename T> inline int round(T value) {
+				return static_cast<int>(std::floor(value + 0.5));
+			}
+
+			/**
+			 * [Copied from CSS3] The Åefont-weightÅf property specifies weight of glyphs in the
+			 * font, their degree of blackness or stroke thickness.
+			 * @see CSS Fonts Module Level 3, 3.2 Font weight: the font-weight property
+			 *      (http://www.w3.org/TR/css3-fonts/#generic-font-families)
+			 * @see Extensible Stylesheet Language (XSL) Version 1.1, 7.9.9 "font-weight"
+			 *      (http://www.w3.org/TR/xsl/#font-weight)
+			 * @see Fonts - SVG 1.1 (Second Edition), 20.8.3 The Åefont-faceÅf element
+			 *      (http://www.w3.org/TR/SVG11/fonts.html#FontFaceElementFontWeightAttribute)
+			 */
+			ASCENSION_BEGIN_SCOPED_ENUM(FontWeight)
+				/// Same as 400.
+				NORMAL = 400,
+				/// Same as 700.
+				BOLD = 700,
+				/// Specifies the weight of the face bolder than the inherited value.
+				BOLDER,
+				/// Specifies the weight of the face lighter than the inherited value.
+				LIGHTER,
+				THIN = 100,
+				EXTRA_LIGHT = 200,
+				ULTRA_LIGHT = 200,
+				LIGHT = 300, 
+				MEDIUM = 500,
+				SEMI_BOLD = 600,
+				DEMI_BOLD = 600,
+				EXTRA_BOLD = 800,
+				ULTRA_BOLD = 800,
+				BLACK = 900,
+				HEAVY = 900
+			ASCENSION_END_SCOPED_ENUM
+
+			/**
+			 * [Copied from CSS3] This property selects a normal, condensed, or expanded face from
+			 * a font family.
+			 * @see CSS Fonts Module Level 3, 3.3 Font width: the font-stretch property
+			 *      (http://www.w3.org/TR/css3-fonts/#font-stretch-prop)
+			 * @see Extensible Stylesheet Language (XSL) Version 1.1, 7.9.5 "font-stretch"
+			 *      (http://www.w3.org/TR/xsl/#font-stretch)
+			 * @see Fonts - SVG 1.1 (Second Edition), 20.8.3 The Åefont-faceÅf element
+			 *      (http://www.w3.org/TR/SVG11/fonts.html#FontFaceElementFontStretchAttribute)
+			 */
+			ASCENSION_BEGIN_SCOPED_ENUM(FontStretch)
+				NORMAL = 1000,			///< Normal.
+//				WIDER,
+//				NARROWER,
+				ULTRA_CONDENSED = 500,	///< Ultra Condensed.
+				EXTRA_CONDENSED = 625,	///< Extra Condensed.
+				CONDENSED = 750,		///< Condensed.
+				SEMI_CONDENSED = 875,	///< Semi Condensed.
+				SEMI_EXPANDED = 1125,	///< Semi Expanded.
+				EXPANDED = 1250,		///< Expanded.
+				EXTRA_EXPANDED = 1500,	///< Extra Expanded.
+				ULTRA_EXPANDED = 2000	///< Ultra Expanded.
+			ASCENSION_END_SCOPED_ENUM
+
+			/**
+			 * [Copied from CSS3] This property allows italic or oblique faces to be selected.
+			 * @see CSS Fonts Module Level 3, 3.4 Font style: the font-style property
+			 *      (http://www.w3.org/TR/css3-fonts/#font-style-prop)
+			 * @see Extensible Stylesheet Language (XSL) Version 1.1, 7.9.7 "font-style"
+			 *      (http://www.w3.org/TR/xsl/#font-style)
+			 * @see Fonts - SVG 1.1 (Second Edition), 20.8.3 The Åefont-faceÅf element
+			 *      (http://www.w3.org/TR/SVG11/fonts.html#FontFaceElementFontStyleAttribute)
+			 */
+			ASCENSION_BEGIN_SCOPED_ENUM(FontStyle)
+				NORMAL,		///< Selects a face that is classified as 'normal'.
+				ITALIC,		///< Selects a face that is labeled 'italic' or 'oblique'.
+				OBLIQUE,	///< Selects a face that is labeled 'oblique'.
+				BACKSLANT	///< Selects a face that is labeled 'backslant'.
+			ASCENSION_END_SCOPED_ENUM
+
+//			ASCENSION_BEGIN_SCOPED_ENUM(FontSynthesis)
+//				NONE = 0,
+//				WEIGHT = 1 << 0,
+//				STYLE = 1 << 1
+//			ASCENSION_END_SCOPED_ENUM
+
+//			ASCENSION_BEGIN_SCOPED_ENUM(FontVariant)
+//				NORMAL,
+//				SMALL_CAPS
+//			ASCENSION_END_SCOPED_ENUM
+
+			ASCENSION_BEGIN_SCOPED_ENUM(FontOrientation)
+				HORIZONTAL,
+				VERTICAL
+			ASCENSION_END_SCOPED_ENUM
+
+			struct FontFeatureSetting {
+				OpenTypeFontTag name;
+				uint32_t value;
+				/// Default constructor does not initialize the data members.
+				FontFeatureSetting() /*noexcept*/ {}
+				FontFeatureSetting(OpenTypeFontTag name, uint32_t value) /*noexcept*/ : name(name), value(value) {}
+			};
+
+			/**
+			 * Set of font properties without the family name.
+			 * @tparam PropertyHolder
+			 * @see FontDescription
+			 */
+			struct FontProperties : private boost::equality_comparable<FontProperties> {
+				FontWeight weight;				///< The font weight.
+				FontStretch stretch;			///< The font stretch.
+				FontStyle style;				///< The font style.
+//				FontVariant variant;			///< The font variant.
+//				FontSynthesis synthesis;		///< The font synthesis.
+				FontOrientation orientation;	///< The font orientation.
+				// TODO: 'font-feature-settings' property should be implemented as TextRunStyle?
+//				std::vector<FontFeatureSetting> featureSettings;
+
+				/**
+				 * Constructor.
+				 * @param weight The initial weight value
+				 * @param stretch The initial stretch value
+				 * @param style The initial style value
+				 * @param orientation The initial orientation value
+				 */
+				explicit FontProperties(FontWeight weight = FontWeight::NORMAL,
+					FontStretch stretch = FontStretch::NORMAL, FontStyle style = FontStyle::NORMAL,
+					FontOrientation orientation = FontOrientation::HORIZONTAL)
+					: weight(weight), stretch(stretch), style(style), orientation(orientation) {}
+				/// Equality operator.
+				bool operator==(const FontProperties& other) const /*noexcept*/ {
+					return weight == other.weight && stretch == other.stretch
+						&& style == other.style && orientation == other.orientation;
+				}
+			};
+
+			class FontDescription : private boost::equality_comparable<FontDescription> {
+			public:
+				/**
+				 * Constructor.
+				 * @param family The font family
+				 * @param pointSize The size in points
+				 * @param properties The other properties
+				 * @throw std#underflow_error @a pointSize is negative
+				 */
+				explicit FontDescription(const FontFamily& family,
+						double pointSize, const FontProperties& properties = FontProperties())
+						: family_(family), pointSize_(pointSize), properties_(properties) {
+					if(pointSize < 0.0)
+						throw std::underflow_error("pointSize");
+				}
+				/// Equality operator.
+				bool operator==(const FontDescription& other) const /*noexcept*/ {
+					return family_ == other.family_
+						&& pointSize_ == other.pointSize_	// TODO: use epsilon.
+						&& properties_ == other.properties_;
+				}
+				/// Returns the font family.
+				const FontFamily& family() const /*noexcept*/ {return family_;}
+				/// Returns the size in points.
+				double pointSize() const /*noexcept*/ {return pointSize_;}
+				/// Returns the other properties.
+				FontProperties& properties() /*noexcept*/ {return properties_;}
+				/// Returns the other properties.
+				const FontProperties& properties() const /*noexcept*/ {return properties_;}
+				/**
+				 * Sets the family name.
+				 * @param family The new font family
+				 */
+				FontDescription& setFamilyName(const FontFamily& family) {
+					return (family_ = family), *this;
+				}
+				/**
+				 * Sets the size in points.
+				 * @param newValue The new size
+				 * @return This object
+				 * @throw std#underflow_error @a newValue is negative
+				 */
+				FontDescription& setPointSize(double newValue) {
+					if(newValue < 0.0)
+						throw std::underflow_error("newValue");
+					return (pointSize_ = newValue), *this;
+				}
+			private:
+				const FontFamily family_;
+				double pointSize_;
+				FontProperties properties_;
+			};
+		}
+	}
+}
+
+namespace std {
+	/// Specialization of @c std#hash class template for @c FontProperties.
+	template<>
+	class hash<ascension::graphics::font::FontProperties> :
+		public std::function<std::hash<void*>::result_type(const ascension::graphics::font::FontProperties&)> {
+	public:
+		result_type operator()(const argument_type& key) const {
+			// TODO: use boost.hash_combine.
+			return std::hash<ascension::graphics::font::FontWeight>()(key.weight)
+				+ std::hash<ascension::graphics::font::FontStretch>()(key.stretch)
+				+ std::hash<ascension::graphics::font::FontStyle>()(key.style)
+//				+ std::hash<ascension::graphics::font::FontVariant>()(key.variant)
+				+ std::hash<ascension::graphics::font::FontOrientation>()(key.orientation);
+		}
+	};
+
+	/// Specialization of @c std#hash class template for @c FontDescription.
+	template<>
+	class hash<ascension::graphics::font::FontDescription> :
+		public std::function<std::hash<void*>::result_type(const ascension::graphics::font::FontDescription&)> {
+	public:
+		result_type operator()(const argument_type& key) const {
+			// TODO: use boost.hash_combine.
+			return std::hash<ascension::graphics::font::FontFamily>()(key.family())
+				+ std::hash<double>()(key.pointSize())
+				+ std::hash<ascension::graphics::font::FontProperties>()(key.properties());
+		}
+	};
+}
+
+#endif // !ASCENSION_FONT_DESCRIPTION_HPP
---
+++ ascension/ascension/graphics/font-family.hpp
@@ -0,0 +1,138 @@
+﻿/**
+ * @file font-family.hpp
+ * @author exeal
+ * @date 2010-11-06 created
+ * @date 2010-2012 was font.hpp
+ * @date 2012-08-19 separated from font.hpp
+ */
+
+#ifndef ASCENSION_FONT_FAMILY_HPP
+#define ASCENSION_FONT_FAMILY_HPP
+
+#include <ascension/config.hpp>	// ASCENSION_VARIATION_SELECTORS_SUPPLEMENT_WORKAROUND
+#include <ascension/platforms.hpp>
+#include <ascension/corelib/basic-exceptions.hpp>	// UnknownValueException
+#include <ascension/corelib/basic-types.hpp>		// uint32_t, ...
+#include <ascension/graphics/geometry.hpp>
+#include <functional>	// std.hash
+#include <memory>		// std.unique_ptr, std.shared_ptr
+#include <boost/operators.hpp>
+#if defined(ASCENSION_SHAPING_ENGINE_CAIRO)
+#	include <cairomm.h>
+#elif defined(ASCENSION_SHAPING_ENGINE_CORE_GRAPHICS)
+#	include <CGFont.h>
+#elif defined(ASCENSION_SHAPING_ENGINE_CORE_TEXT)
+#	include <CTFont.h>
+#elif defined(ASCENSION_SHAPING_ENGINE_DIRECT_WRITE)
+#	include <dwrite.h>
+#elif defined(ASCENSION_SHAPING_ENGINE_HARFBUZZ)
+#	include <hb.h>
+#	include <boost/intrusive_ptr.hpp>
+namespace boost {
+	inline void intrusive_ptr_add_ref(hb_font_t* p) {::hb_font_reference(p);}
+	inline void intrusive_ptr_release(hb_font_t* p) {::hb_font_destroy(p);}
+}
+#elif defined(ASCENSION_SHAPING_ENGINE_PANGO)
+#	include <pangomm.h>
+#elif defined(ASCENSION_SHAPING_ENGINE_QT)
+#	include <QFont>
+#elif defined(ASCENSION_SHAPING_ENGINE_UNISCRIBE) || defined(ASCENSION_SHAPING_ENGINE_WIN32_GDI)
+#	include <ascension/win32/handle.hpp>	// win32.Handle
+#elif defined(ASCENSION_SHAPING_ENGINE_WIN32_GDIPLUS)
+#	include <GdiPlus.h>
+#endif
+
+namespace ascension {
+	namespace graphics {
+
+		/**
+		 * @a font namespace is...
+		 * Font properties specifications are designed based on "CSS Fonts Module Level 3"
+		 * (http://dev.w3.org/csswg/css3-fonts/).
+		 */
+		namespace font {
+			/**
+			 * @c FontFamily represents a family of related font faces. A font family is a group of
+			 * font faces that share a common design, but differ in styles.
+			 * @see CSS Fonts Module Level 3, 3.1 Font family: the font-family property
+			 *      (http://www.w3.org/TR/css3-fonts/#font-family-prop)
+			 * @see CSS Fonts Module Level 3, 4.2 Font family: the font-family descriptor
+			 *      (http://www.w3.org/TR/css3-fonts/#font-family-desc)
+			 * @see Extensible Stylesheet Language (XSL) Version 1.1, 7.9.2 "font-family"
+			 *      (http://www.w3.org/TR/xsl/#font-family)
+			 * @see Fonts - SVG 1.1 (Second Edition), 20.8.3 The ‘font-face’ element
+			 *      (http://www.w3.org/TR/SVG11/fonts.html#FontFaceElementFontFamilyAttribute)
+			 * @see FontFace, fontFaces
+			 */
+			class FontFamily : private boost::totally_ordered<FontFamily> {
+			public:
+				/**
+				 * @see CSS Fonts Module Level 3, 3.1.1 Generic font families
+				 *      (http://www.w3.org/TR/css3-fonts/#generic-font-families)
+				 */
+				enum GenericFamily {
+					SERIF, SANS_SERIF, CURSIVE, FANTASY, MONOSPACE
+				};
+			public:
+#if defined(ASCENSION_SHAPING_ENGINE_DIRECT_WRITE)
+				explicit FontFamily(const String& name);
+				explicit FontFamily(win32::com::SmartPointer<IDWriteFontFamily> nativeObject);
+				win32::com::SmartPointer<IDWriteFontFamily> asNativeObject() const;
+#elif defined(ASCENSION_SHAPING_ENGINE_PANGO)
+				explicit FontFamily(const String& name);
+				explicit FontFamily(Glib::RefPtr<Pango::FontFamily> nativeObject);
+				Glib::RefPtr<Pango::FontFamily> asNativeObject();
+				Glib::RefPtr<const Pango::FontFamily> asNativeObject() const;
+#elif defined(ASCENSION_SHAPING_ENGINE_WIN32_GDIPLUS)
+				explicit FontFamily(const String& name);
+				explicit FontFamily(std::unique_ptr<Gdiplus::FontFamily>&& nativeObject);
+				explicit FontFamily(std::shared_ptr<Gdiplus::FontFamily> nativeObject);
+				explicit FontFamily(Gdiplus::FontFamily& nativeObject);	// weak ref.
+				std::shared_ptr<Gdiplus::FontFamily> asNativeObject() /*noexcept*/;
+				std::shared_ptr<const Gdiplus::FontFamily> asNativeObject() const /*noexcept*/;
+#else
+				explicit FontFamily(const String& name) : name_(name) {
+					if(name.empty())
+						throw std::length_error("name");
+				}
+#endif
+				explicit FontFamily(GenericFamily genericFamily);
+				const String& name() const /*noexcept*/ {return name_;}
+			private:
+#if defined(ASCENSION_SHAPING_ENGINE_DIRECT_WRITE)
+				win32::com::SmartPointer<IDWriteFontFamily> nativeObject_;
+#elif defined(ASCENSION_SHAPING_ENGINE_PANGO)
+				Glib::RefPtr<Pango::FontFamily> nativeObject_;
+#elif defined(ASCENSION_SHAPING_ENGINE_WIN32_GDIPLUS)
+				std::shared_ptr<Gdiplus::FontFamily> nativeObject_;
+#else
+				const String name_;
+#endif
+			};
+
+			/// Equality operator for @c FontFamily.
+			inline bool operator==(const FontFamily& lhs, const FontFamily& rhs) /*noexcept*/ {
+				return lhs.name() == rhs.name();
+			}
+
+			/// Less-than operator for @c FontFamily.
+			inline bool operator<(const FontFamily& lhs, const FontFamily& rhs) /*noexcept*/ {
+				return lhs.name() < rhs.name();
+			}
+		}
+	}
+}
+
+namespace std {
+	/// Specialization of @c std#hash class template for @c FontFamily.
+	template<>
+	class hash<ascension::graphics::font::FontFamily> : public std::hash<ascension::String> {
+	public:
+		typedef ascension::graphics::font::FontFamily argument_type;
+		result_type operator()(const argument_type& key) const {
+			return std::hash<ascension::String>::operator()(key.name());
+		}
+	};
+}
+
+#endif // !ASCENSION_FONT_FAMILY_HPP
