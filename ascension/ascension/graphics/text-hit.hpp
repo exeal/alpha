90da6996ce8c6cb9ed450016b302e8c7afa578bb
exeal <exeal@users.sourceforge.jp>
2013-01-27 01:40:35 +0900

    Renamed graphics.font.TextHitInformation class TextHit.

--- ascension/ascension/graphics/text-hit.hpp
+++ ascension/ascension/graphics/text-hit.hpp
@@ -1,13 +1,14 @@
 /**
- * @file text-hit-information.hpp
- * Defines @c ascension#graphics#font#TextHitInformation class.
+ * @file text-hit.hpp
+ * Defines @c ascension#graphics#font#TextHit class.
  * @author exeal
  * @date 2012-11-04 created
+ * @date 2013-01-27 renamed from text-hit-information.hpp
  * @date 2012-2013
  */
 
-#ifndef ASCENSION_TEXT_HIT_INFORMATION_HPP
-#define ASCENSION_TEXT_HIT_INFORMATION_HPP
+#ifndef ASCENSION_TEXT_HIT_HPP
+#define ASCENSION_TEXT_HIT_HPP
 #include <ascension/corelib/basic-types.hpp>	// Index, SignedIndex, BOOST_NOEXCEPT
 #include <numeric>		// std.numeric_limits
 #include <stdexcept>	// std.overflow_error, std.underflow_error
@@ -16,61 +17,70 @@
 namespace ascension {
 	namespace graphics {
 		namespace font {
-			class TextHitInformation : private boost::totally_ordered<TextHitInformation> {
+			/**
+			 * Represents a character position in a text layout, and a <strong>bias</strong> or
+			 * side of the character. Biases are either <em>leading</em> (the left edge, for a
+			 * left-to-right character) or <em>trailing</em> (the right edge, for a
+			 * left-to-right character). Instances of @c TextHit are used to specify caret and
+			 * insertion positions within text.
+			 * @see TextLayout
+			 * @note This class is designed based on @c java.awt.font.TextHitInfo class in Java.
+			 */
+			class TextHit : private boost::totally_ordered<TextHit> {
 			public:
 				/// @name Factories
 				/// @{
 				/**
-				 * Creates a @c TextHitInformation at the specified offset, associated with the
-				 * character after the offset.
+				 * Creates a @c TextHit at the specified offset, associated with the character
+				 * after the offset.
 				 * @param offset An offset associated with the character after the offset
-				 * @return A @c TextHitInformation at the specified offset
+				 * @return A @c TextHit at the specified offset
 				 * @see #beforeOffset
 				 */
-				static TextHitInformation afterOffset(Index offset) BOOST_NOEXCEPT {
-					return TextHitInformation(offset, true);
+				static TextHit afterOffset(Index offset) BOOST_NOEXCEPT {
+					return TextHit (offset, true);
 				}
 				/**
-				 * Creates a @c TextHitInformation at the specified offset, associated with the
-				 * character before the offset.
+				 * Creates a @c TextHit at the specified offset, associated with the character
+				 * before the offset.
 				 * @param offset An offset associated with the character before the offset
-				 * @return A @c TextHitInformation at the specified offset
+				 * @return A @c TextHit at the specified offset
 				 * @see #afterOffset
 				 */
-				static TextHitInformation beforeOffset(Index offset) BOOST_NOEXCEPT {
-					return TextHitInformation(offset - 1, false);
+				static TextHit beforeOffset(Index offset) BOOST_NOEXCEPT {
+					return TextHit(offset - 1, false);
 				}
 				/**
-				 * Creates a @c TextHitInformation on the leading edge of the character at the
-				 * specified @a characterIndex.
+				 * Creates a @c TextHit on the leading edge of the character at the specified
+				 * @a characterIndex.
 				 * @param characterIndex The index of the character hit
-				 * @return A @c TextHitInformation on the leading edge of the character at the
+				 * @return A @c TextHit on the leading edge of the character at the
 				 *         specified @a characterIndex
 				 */
-				static TextHitInformation leading(Index characterIndex) BOOST_NOEXCEPT {
-					return TextHitInformation(characterIndex, true);
+				static TextHit leading(Index characterIndex) BOOST_NOEXCEPT {
+					return TextHit(characterIndex, true);
 				}
 				/**
-				 * Creates a @c TextHitInformation on the trailing edge of the character at the
-				 * specified @a characterIndex.
+				 * Creates a @c TextHit on the trailing edge of the character at the specified
+				 * @a characterIndex.
 				 * @param characterIndex The index of the character hit
-				 * @return A @c TextHitInformation on the trailing edge of the character at the
+				 * @return A @c TextHit on the trailing edge of the character at the
 				 *         specified @a characterIndex
 				 */
-				static TextHitInformation trailing(Index characterIndex) BOOST_NOEXCEPT {
-					return TextHitInformation(characterIndex, false);
+				static TextHit trailing(Index characterIndex) BOOST_NOEXCEPT {
+					return TextHit(characterIndex, false);
 				}
 				/// @}
 
 				/// @name Relational Operators
 				/// @{
 				/** Equality operator. */
-				bool operator==(const TextHitInformation& other) const BOOST_NOEXCEPT {
+				bool operator==(const TextHit& other) const BOOST_NOEXCEPT {
 					return characterIndex() == other.characterIndex()
 						&& isLeadingEdge() == other.isLeadingEdge();
 				}
 				/// Less-than operator.
-				bool operator<(const TextHitInformation& other) const BOOST_NOEXCEPT {
+				bool operator<(const TextHit& other) const BOOST_NOEXCEPT {
 					return characterIndex() < other.characterIndex()
 						|| (characterIndex() == other.characterIndex() && isLeadingEdge() && !other.isLeadingEdge());
 				}
@@ -99,45 +109,44 @@ namespace ascension {
 				/// @name Other Factories
 				/// @{
 				/**
-				 * Creates a @c TextHitInformation whose character index is offset by @a delta from
-				 * the @c #characterIndex of this @c TextHitInformation. This @c TextHitInformation
-				 * remains unchanged.
+				 * Creates a @c TextHit whose character index is offset by @a delta from the
+				 * @c #characterIndex of this @c TextHit. This @c TextHit remains unchanged.
 				 * @param delta The value to offset this @c #characterIndex
-				 * @return A @c TextHitInformation whose @c #characterIndex is offset by @a delta
-				 *         from the @c #characterIndex of this @c TextHitInformation
+				 * @return A @c TextHit whose @c #characterIndex is offset by @a delta
+				 *         from the @c #characterIndex of this @c TextHit
 				 * @throw std#overflow_error
 				 * @throw std#underflow_error
 				 * @see #otherHit
 				 */
-				TextHitInformation offsetHit(SignedIndex delta) const {
+				TextHit offsetHit(SignedIndex delta) const {
 					if(delta > 0 && static_cast<Index>(delta) > std::numeric_limits<Index>::max() - characterIndex())
 						throw std::overflow_error("delta");
 					else if(delta < 0 && static_cast<Index>(-delta) > characterIndex())
 						throw std::underflow_error("delta");
-					return TextHitInformation(characterIndex() + delta, isLeadingEdge());
+					return TextHit(characterIndex() + delta, isLeadingEdge());
 				}
 				/**
-				 * Creates a @c TextHitInformation on the other side of the insertion point. This
-				 * @c TextHitInformation remains unchanged.
-				 * @return A @c TextHitInformation on the other side of the insertion point
+				 * Creates a @c TextHit on the other side of the insertion point. This @c TextHit
+				 * remains unchanged.
+				 * @return A @c TextHit on the other side of the insertion point
 				 * @see #offsetHit
 				 */
-				TextHitInformation otherHit() const BOOST_NOEXCEPT {
+				TextHit otherHit() const BOOST_NOEXCEPT {
 					return isLeadingEdge() ?
 						trailing(characterIndex() - 1) : leading(characterIndex() + 1);
 				}
 				/// @}
 			private:
-				TextHitInformation(Index characterIndex, bool isLeadingEdge) BOOST_NOEXCEPT
+				TextHit(Index characterIndex, bool isLeadingEdge) BOOST_NOEXCEPT
 					: characterIndex_(characterIndex), isLeadingEdge_(isLeadingEdge) {}
 				Index characterIndex_;
 				bool isLeadingEdge_;
 			};
 
 			template<typename Character, Traits>
-			inline std::basic_ostream<Character, Traits>& operator<<(std::basic_ostream<Character, Traits>& out, const TextHitInformation& v) {
+			inline std::basic_ostream<Character, Traits>& operator<<(std::basic_ostream<Character, Traits>& out, const TextHit& v) {
 				std::ostringstream ss;
-				ss << "TextHitInformation[" << v.characterIndex() << (v.isLeadingEdge() ? "L]" : "T]");
+				ss << "TextHit[" << v.characterIndex() << (v.isLeadingEdge() ? "L]" : "T]");
 				const std::string s(ss.str());
 				return out << std::basic_string<Character, Traits>(std::begin(s), std::end(s));
 			}
@@ -145,4 +154,4 @@ namespace ascension {
 	}
 }
 
-#endif // !ASCENSION_TEXT_HIT_INFORMATION_HPP
+#endif // !ASCENSION_TEXT_HIT_HPP
