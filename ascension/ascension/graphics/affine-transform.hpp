c790fd764153e1ac16687bf3c30be44327c7c113
exeal <luigi.irmr@gmail.com>
2013-02-10 22:44:39 +0900

    Added intrinsic ascension.graphics.geometry.AffineTransform class.

--- ascension/ascension/graphics/affine-transform.hpp
+++ ascension/ascension/graphics/affine-transform.hpp
@@ -7,6 +7,7 @@
 #ifndef ASCENSION_AFFINE_TRANSFORM_HPP
 #define ASCENSION_AFFINE_TRANSFORM_HPP
 #include <ascension/graphics/geometry.hpp>
+//#include <boost/math/constants/constants.hpp>	// boost.math.constants.pi
 #if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
 #	include <cairomm/matrix.h>
 #elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
@@ -20,259 +21,165 @@
 namespace ascension {
 	namespace graphics {
 		namespace geometry {
+#ifndef ASCENSION_DOXYGEN_SHOULD_SKIP_THIS
+			BOOST_PARAMETER_NAME(sx)
+			BOOST_PARAMETER_NAME(shy)
+			BOOST_PARAMETER_NAME(shx)
+			BOOST_PARAMETER_NAME(sy)
+			BOOST_PARAMETER_NAME(tx)
+			BOOST_PARAMETER_NAME(ty)
+#endif	// !ASCENSION_DOXYGEN_SHOULD_SKIP_THIS
 
-			/// Affine transform identifying tag.
-			struct AffineTransformTag {};
-
-			const std::size_t SCALE_X_ELEMENT = 2;
-			const std::size_t SCALE_Y_ELEMENT = 3;
-			const std::size_t SHEAR_X_ELEMENT = 4;
-			const std::size_t SHEAR_Y_ELEMENT = 5;
-			const std::size_t TRANSLATE_X_ELEMENT = 6;
-			const std::size_t TRANSLATE_Y_ELEMENT = 7;
-
-#if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
-			namespace nativetypes {typedef Cairo::Matrix NativeAffineTransform;}
-			template<> struct Coordinate<Cairo::Matrix> {typedef double Type;};
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
-			namespace nativetypes {typedef CGAffineTransform NativeAffineTransform;}
-			template<> struct Coordinate<CGAffineTransform> {typedef CGFloat Type;};
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_QT)
-			namespace nativetypes {typedef QMatrix NativeAffineTransform;}
-			template<> struct Coordinate<QMatrix> {typedef qreal Type;};
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_WIN32_GDI)
-			namespace nativetypes {typedef XFORM NativeAffineTransform;}
-			template<> struct Coordinate<XFORM> {typedef FLOAT Type;};
-#endif
-			template<> struct Tag<nativetypes::NativeAffineTransform> {
-				typedef AffineTransformTag Type;
+			/// Base type for @c AffineTransform class.
+			class AffineTransformBase {
+			public:
+				typedef double value_type;
+			public:
+				/// @name Coordinate Accesses
+				/// @{
+				value_type scaleX() const BOOST_NOEXCEPT;
+				value_type scaleY() const BOOST_NOEXCEPT;
+				value_type shearX() const BOOST_NOEXCEPT;
+				value_type shearY() const BOOST_NOEXCEPT;
+				value_type translateX() const BOOST_NOEXCEPT;
+				value_type translateY() const BOOST_NOEXCEPT;
+				/// @}
+			protected:
+				AffineTransformBase() BOOST_NOEXCEPT;
+				AffineTransformBase(value_type sx, value_type shy, value_type shx,
+					value_type sy, value_type tx, value_type ty) BOOST_NOEXCEPT;
+				template<typename Arguments>
+				AffineTransformBase(const Arguments& arguments);
+			private:
+				value_type scaleX_, scaleY_, shearX_, shearY_, translateX_, translateY_;
 			};
 
-			namespace traits {
-				template<typename AffineTransform> struct Maker<AffineTransformTag, AffineTransform> {
-					static AffineTransform make(
-							typename Coordinate<AffineTransform>::Type scaleX,
-							typename Coordinate<AffineTransform>::Type scaleY,
-							typename Coordinate<AffineTransform>::Type shearX,
-							typename Coordinate<AffineTransform>::Type shearY,
-							typename Coordinate<AffineTransform>::Type translateX,
-							typename Coordinate<AffineTransform>::Type translateY) {
+			/// Represents a 2D affine transform.
+			class AffineTransform : public AffineTransformBase {
+			public:
+				/// Constructs a new @c AffineTransform representing the Identity transformation.
+				AffineTransform() {}
+				BOOST_PARAMETER_CONSTRUCTOR(
+					AffineTransform, (AffineTransformBase), tag,
+					(required
+						(sx, (value_type))
+						(shy, (value_type))
+						(shx, (value_type))
+						(sy, (value_type))
+						(tx, (value_type))
+						(ty, (value_type))))
+
 #if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
-						return Cairomm::Matrix(scaleX, shearY, shearX, scaleY, translateX, translateY);
+				AffineTransform(const Cairo::Matrix& native) :
+					AffineTransformBase(native.xx, native.yx, native.xy, native.yy, native.x0, native.y0) {}
+				operator Cairo::Matrix() const {
+					return Cairo::Matrix(scaleX(), shearY(), shearX(), scaleY(), translateX(), translateY());
+				}
 #elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
-						return ::CGAffineTransformMake(scaleX, shearY, shearX, scaleY, translateX, translateY);
+				AffineTransform(const CGAffineTransform& native) :
+					AffineTransformBase(native.a, native.b, native.c, native.d, native.tx, native.ty) {}
+				operator CGAffineTransform() const {
+					return ::CGAffineTransformMake(scaleX(), shearY(), shearX(), scaleY(), translateX(), translateY());
+				}
 #elif defined(ASCENSION_GRAPHICS_SYSTEM_QT)
-						return QMatrix(scaleX, shearY, shearX, scaleY, translateX, translateY);
+				AffineTransform(const QMatrix& native) :
+					AffineTransformBase(native.m11(), native.m21(), native.m12(), native.m22, native.dx(), native.dy()) {}
+				operator QMatrix() const {
+					return QMatrix(scaleX(), shearY(), shearX(), scaleY(), translateX(), translateY());
+				}
 #elif defined(ASCENSION_GRAPHICS_SYSTEM_WIN32_GDI)
-						XFORM temp = {scaleX, shearX, shearY, scaleY, translateX, translateY};
-						return temp;
+				AffineTransform(const XFORM& native) :
+					AffineTransformBase(native.eM11, native.eM21, native.eM12, native.eM22, native.eDx, native.eDy) {}
+				operator XFORM() const {
+					XFORM temp = {
+						static_cast<FLOAT>(scaleX()), static_cast<FLOAT>(shearX()),
+						static_cast<FLOAT>(shearY()), static_cast<FLOAT>(scaleY()),
+						static_cast<FLOAT>(translateX()), static_cast<FLOAT>(translateY())
+					};
+					return temp;
+				}
+#else
 #endif
-					}
-				};
-				template<typename AffineTransform>
-				struct Accessor<AffineTransformTag, AffineTransform, SCALE_X_ELEMENT> {
-					static typename Coordinate<AffineTransform>::Type get(const AffineTransform& a) {
-#if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
-						return a.xx;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
-						return a.a;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_QT)
-						return a.m11();
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_WIN32_GDI)
-						return a.eM11;
-#endif
-					}
-				};
-				template<typename AffineTransform>
-				struct Accessor<AffineTransformTag, AffineTransform, SCALE_Y_ELEMENT> {
-					static typename Coordinate<AffineTransform>::Type get(const AffineTransform& a) {
-#if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
-						return a.yy;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
-						return a.d;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_QT)
-						return a.m22();
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_WIN32_GDI)
-						return a.eM22;
-#endif
-					}
-				};
-				template<typename AffineTransform>
-				struct Accessor<AffineTransformTag, AffineTransform, SHEAR_X_ELEMENT> {
-					static typename Coordinate<AffineTransform>::Type get(const AffineTransform& a) {
-#if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
-						return a.xy;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
-						return a.c;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_QT)
-						return a.m12();
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_WIN32_GDI)
-						return a.eM12;
-#endif
-					}
-				};
-				template<typename AffineTransform>
-				struct Accessor<AffineTransformTag, AffineTransform, SHEAR_Y_ELEMENT> {
-					static typename Coordinate<AffineTransform>::Type get(const AffineTransform& a) {
-#if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
-						return a.yx;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
-						return a.b;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_QT)
-						return a.m21();
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_WIN32_GDI)
-						return a.eM21;
-#endif
-					}
-				};
-				template<typename AffineTransform>
-				struct Accessor<AffineTransformTag, AffineTransform, TRANSLATE_X_ELEMENT> {
-					static typename Coordinate<AffineTransform>::Type get(const AffineTransform& a) {
-#if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
-						return a.x0;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
-						return a.tx;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_QT)
-						return a.dx();
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_WIN32_GDI)
-						return a.eDx;
-#endif
-					}
-				};
-				template<typename AffineTransform>
-				struct Accessor<AffineTransformTag, AffineTransform, TRANSLATE_Y_ELEMENT> {
-					static typename Coordinate<AffineTransform>::Type get(const AffineTransform& a) {
-#if defined(ASCENSION_GRAPHICS_SYSTEM_CAIRO)
-						return a.y0;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_CORE_GRAPHICS)
-						return a.ty;
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_QT)
-						return a.dy();
-#elif defined(ASCENSION_GRAPHICS_SYSTEM_WIN32_GDI)
-						return a.eDy;
-#endif
-					}
-				};
-			} // namespace traits
 
-			// known transformations //////////////////////////////////////////////////////////////
+				/// @name Factories For Known Transformations
+				/// @{
+				static AffineTransform&& quadrantRotation(int numberOfQuadrants);
+				template<typename Geometry>
+				static AffineTransform&& quadrantRotation(int numberOfQuadrants, const Geometry& anchor,
+					typename detail::EnableIfTagIs<Geometry, boost::geometry::point_tag>::type* = nullptr);
+				static AffineTransform&& rotation(value_type thetaInRadians);
+				template<typename Geometry>
+				static AffineTransform&& rotation(value_type thetaInRadians, const Geometry& anchor,
+					typename detail::EnableIfTagIs<Geometry, boost::geometry::point_tag>::type* = nullptr);
+				static AffineTransform&& rotation(const BasicDimension<value_type>& rotationVector);
+				template<typename Geometry>
+				static AffineTransform&& rotation(
+					const BasicDimension<value_type>& rotationVector, const Geometry& anchor,
+					typename detail::EnableIfTagIs<Geometry, boost::geometry::point_tag>::type* = nullptr);
+				static AffineTransform&& scaling(value_type sx, value_type sy) BOOST_NOEXCEPT;
+				static AffineTransform&& shearing(value_type shx, value_type shy) BOOST_NOEXCEPT;
+				static AffineTransform&& translation(value_type tx, value_type ty) BOOST_NOEXCEPT;
+				/// @}
 
-			template<typename AffineTransform>
-			inline AffineTransform identityTransform();
-			template<typename AffineTransform>
-			inline AffineTransform quadrantRotationTransform(int numberOfQuadrants);
-			template<typename AffineTransform, typename Point>
-			inline AffineTransform quadrantRotationTransform(int numberOfQuadrants, const Point& anchor);
-			template<typename AffineTransform>
-			inline AffineTransform rotationTransform(double thetaInRadians);
-			template<typename AffineTransform, typename Point>
-			inline AffineTransform rotationTransform(
-				typename Coordinate<AffineTransform>::Type thetaInRadians, const Point& anchor);
-			template<typename AffineTransform>
-			inline AffineTransform rotationTransform(
-				typename Coordinate<AffineTransform>::Type vectorX,
-				typename Coordinate<AffineTransform>::Type vectorY);
-			template<typename AffineTransform, typename Point>
-			inline AffineTransform rotationTransform(
-				typename Coordinate<AffineTransform>::Type vectorX,
-				typename Coordinate<AffineTransform>::Type vectorY, const Point& anchor);
-			template<typename AffineTransform>
-			inline AffineTransform scalingTransform(
-				typename Coordinate<AffineTransform>::Type sx,
-				typename Coordinate<AffineTransform>::Type sy);
-			template<typename AffineTransform>
-			inline AffineTransform shearingTransform(
-				typename Coordinate<AffineTransform>::Type shx,
-				typename Coordinate<AffineTransform>::Type shy);
-			template<typename AffineTransform>
-			inline AffineTransform translationTransform(
-				typename Coordinate<AffineTransform>::Type tx,
-				typename Coordinate<AffineTransform>::Type ty);
+				/// @name Attributes
+				/// @{
+				value_type determinant() const BOOST_NOEXCEPT;
+				bool isIdentity() const BOOST_NOEXCEPT;
+				/// @}
 
-			// attributes /////////////////////////////////////////////////////////////////////////
+				/// @name Inverse Transformations
+				/// @{
+				AffineTransform& invert();
+//				bool isInvertible() const;
+				/// @}
 
-			template<typename AffineTransform>
-			inline NativeSize deltaTransform(const AffineTransform& a);
-			template<typename AffineTransform>
-			inline typename Coordinate<AffineTransform>::Type determinant(const AffineTransform& a);
-			template<typename AffineTransform>
-			inline bool isIdentity(const AffineTransform& a,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
+				/// @name Generic Concatenations
+				/// @{
+				AffineTransform& concatenate(const AffineTransform& tx);
+				AffineTransform& preConcatenate(const AffineTransform& tx);
+				/// @}
 
-			// inverse transformations ////////////////////////////////////////////////////////////
+				/// @name Known Concatenations
+				/// @{
+				AffineTransform& quadrantRotate(int numberOfQuadrants);
+				template<typename Geometry>
+				AffineTransform& quadrantRotate(int numberOfQuadrants, const Geometry& anchor,
+					typename detail::EnableIfTagIs<Geometry, boost::geometry::point_tag>::type* = nullptr);
+				AffineTransform& rotate(value_type thetaInRadians);
+				template<typename Geometry>
+				AffineTransform& rotate(value_type thetaInRadians, const Geometry& anchor,
+					typename detail::EnableIfTagIs<Geometry, boost::geometry::point_tag>::type* = nullptr);
+				inline AffineTransform& rotate(const BasicDimension<value_type>& rotationVector);
+				template<typename Geometry>
+				inline AffineTransform& rotate(
+					const BasicDimension<value_type>& rotationVector, const Geometry& anchor,
+					typename detail::EnableIfTagIs<Geometry, boost::geometry::point_tag>::type* = nullptr);
+				AffineTransform& scale(value_type sx, value_type sy);
+//				AffineTransform& scaleNonUniform(value_type sx, value_type sy);
+				AffineTransform& shear(value_type shx, value_type shy);
+				AffineTransform& translate(value_type tx, value_type ty);
+				/// @}
 
-			template<typename AffineTransform>
-			inline AffineTransform inverted(const AffineTransform& a,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline bool isInvertible(const AffineTransform& a,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
+				/// @name Transformations
+				/// @{
+				template<typename Geometry>
+				Geometry&& deltaTransform(const Geometry& p,
+					typename detail::EnableIfTagIs<Geometry, boost::geometry::point_tag>::type* = nullptr) const;
+				template<typename Geometry>
+				Geometry&& inverseTransform(const Geometry& p,
+					typename detail::EnableIfTagIs<Geometry, boost::geometry::point_tag>::type* = nullptr) const;
+				template<typename Geometry>
+				Geometry& transformShape(const AffineTransform& tx, Geometry& shape);
+				template<typename Geometry>
+				Geometry&& transformedShape(const AffineTransform& tx, const Geometry& shape);
+				/// @}
+			};
 
-			// generic concatenations /////////////////////////////////////////////////////////////
-
-			template<typename AffineTransform>
-			inline AffineTransform concatenate(
-				AffineTransform& lhs, const AffineTransform& rhs,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform preConcatenate(
-				const AffineTransform& lhs, AffineTransform& rhs,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-
-			// known concatenations ///////////////////////////////////////////////////////////////
-
-			template<typename AffineTransform>
-			inline AffineTransform& quadrantRotate(AffineTransform& a, int numberOfQuadrants,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform& quadrantRotate(AffineTransform& a, int numberOfQuadrants,
-				typename Coordinate<AffineTransform>::Type x,
-				typename Coordinate<AffineTransform>::Type y,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform& rotate(AffineTransform& a,
-				typename Coordinate<AffineTransform>::Type thetaInRadians,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform& rotate(AffineTransform& a,
-				typename Coordinate<AffineTransform>::Type thetaInRadians,
-				typename Coordinate<AffineTransform>::Type x,
-				typename Coordinate<AffineTransform>::Type y,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform& scale(AffineTransform& a,
-				typename Coordinate<AffineTransform>::Type sx,
-				typename Coordinate<AffineTransform>::Type sy,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform& scaleNonUniform(AffineTransform& a,
-				typename Coordinate<AffineTransform>::Type sx,
-				typename Coordinate<AffineTransform>::Type sy,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform& scale(AffineTransform& a,
-				typename Coordinate<AffineTransform>::Type factor,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform& shear(AffineTransform& a,
-				typename Coordinate<AffineTransform>::Type shx,
-				typename Coordinate<AffineTransform>::Type shy,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-			template<typename AffineTransform>
-			inline AffineTransform& translate(AffineTransform& a,
-				typename Coordinate<AffineTransform>::Type tx,
-				typename Coordinate<AffineTransform>::Type ty,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-
-			// transformations ////////////////////////////////////////////////////////////////////
-
-			template<typename AffineTransform, typename Geometry>
-			inline Geometry& transform(const AffineTransform& a, Geometry& g,
-				typename detail::EnableIfTagIs<AffineTransform, AffineTransformTag>::type* = nullptr);
-
+//			/// Affine transform identifying tag.
+//			struct AffineTransformTag {};
 		}
 
-		using namespace geometry::nativetypes;
+		using geometry::AffineTransform;
 	}
 }
 
