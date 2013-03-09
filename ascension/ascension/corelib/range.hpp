27dfa05729d309a8597a7e06d8198be333608b3a
exeal <exeal@users.sourceforge.jp>
2013-03-10 02:18:09 +0900

    Added includes, intersection, intersects and ordered functions in range.hpp.

--- ascension/ascension/corelib/range.hpp
+++ ascension/ascension/corelib/range.hpp
@@ -112,10 +112,20 @@ namespace ascension {
 			typename boost::range_iterator<RandomAccessTraversalRange>::type position) {
 		return position >= boost::const_begin(range) && position < boost::const_end(range);
 	}
+	template<typename RandomAccessTraversalRange, typename BinaryPredicate>
+	inline bool includes(const RandomAccessTraversalRange& range,
+			typename boost::range_iterator<RandomAccessTraversalRange>::type position, BinaryPredicate pred) {
+		return !pred(position, range.begin()) && pred(position, range.end());
+	}
 	template<typename Integer>
 	inline bool includes(const boost::integer_range<Integer>& range,
 			typename boost::integer_range<Integer>::value_type value) {
 		return value >= *range.begin() && value < *range.end();
+	}
+	template<typename Integer, typename BinaryPredicate>
+	inline bool includes(const boost::integer_range<Integer>& range,
+			typename boost::integer_range<Integer>::value_type value, BinaryPredicate pred) {
+		return !pred(value, *range.begin()) && pred(value, *range.end());
 	}
 #endif	// ASCENSION_ABANDONED_AT_VERSION_08
 
@@ -140,9 +150,17 @@ namespace ascension {
 	inline bool includes(const RandomAccessTraversalRange1& range, const RandomAccessTraversalRange2& subrange) {
 		return boost::const_begin(subrange) >= boost::const_begin(range) && boost::const_end(subrange) <= boost::const_begin(range);
 	}
+	template<typename RandomAccessTraversalRange1, typename RandomAccessTraversalRange2, typename BinaryPredicate>
+	inline bool includes(const RandomAccessTraversalRange1& range, const RandomAccessTraversalRange2& subrange, BinaryPredicate pred) {
+		return !pred(boost::const_begin(subrange), boost::const_begin(range)) && !pred(boost::const_end(range), boost::const_end(subrange));
+	}
 	template<typename Integer1, typename Integer2>
 	inline bool includes(const boost::integer_range<Integer1>& range, const boost::integer_range<Integer2>& subrange) {
 		return *subrange.begin() >= *range.begin() && *subrange.end() <= *range.end();
+	}
+	template<typename Integer1, typename Integer2, typename BinaryPredicate>
+	inline bool includes(const boost::integer_range<Integer1>& range, const boost::integer_range<Integer2>& subrange, BinaryPredicate pred) {
+		return !pred(*subrange.begin(), *range.begin()) && !pred(*range.end(), *subrange.end());
 	}
 #endif	// ASCENSION_ABANDONED_AT_VERSION_08
 
@@ -155,16 +173,47 @@ namespace ascension {
 	 * @param range The range
 	 * @param other The other range
 	 * @return The intersection, or empty range if the two ranges were not intersected
-	 * @see intersected, merged
+	 * @see intersects, merged
 	 */
 	template<typename T, typename Comp, typename Other>
-	inline Range<T, Comp> intersected(const Range<T, Comp>& range, const Range<Other, Comp>& other) {
+	inline Range<T, Comp> intersection(const Range<T, Comp>& range, const Range<Other, Comp>& other) {
 		const Comp lessThan;
 		const T b(std::max<T, Comp>(range.beginning(), other.beginning(), lessThan));
 		const T e(std::min<T, Comp>(range.end(), other.end(), lessThan));
 		return Range<T, Comp>(b, std::max(b, e, lessThan));
 	}
 #else
+	template<typename RandomAccessTraversalRange>
+	inline RandomAccessTraversalRange intersection(
+			const RandomAccessTraversalRange& range1, const RandomAccessTraversalRange& range2) {
+		const typename boost::range_iterator<RandomAccessTraversalRange>::type
+			b(std::max(boost::const_begin(range1), boost::const_begin(range2))),
+			e(std::min(boost::const_end(range1), boost::const_end(range2)));
+		return boost::make_iterator_range(b, std::max(b, e));
+	}
+	template<typename RandomAccessTraversalRange, typename BinaryPredicate>
+	inline RandomAccessTraversalRange intersection(
+			const RandomAccessTraversalRange& range1, const RandomAccessTraversalRange& range2,
+			BinaryPredicate pred) {
+		const typename boost::range_iterator<RandomAccessTraversalRange>::type
+			b(std::max(boost::const_begin(range1), boost::const_begin(range2), pred)),
+			e(std::min(boost::const_end(range1), boost::const_end(range2), pred));
+		return boost::make_iterator_range(b, std::max(b, e, pred));
+	}
+	template<typename Integer>
+	inline boost::integer_range<Integer> intersection(
+			const boost::integer_range<Integer>& range1, const boost::integer_range<Integer>& range2) {
+		const Integer b(std::max(*range1.begin(), *range2.begin())), e(std::min(*range1.end(), *range2.end()));
+		return boost::irange(b, std::max(b, e));
+	}
+	template<typename Integer, typename BinaryPredicate>
+	inline boost::integer_range<Integer> intersection(const boost::integer_range<Integer>& range1,
+			const boost::integer_range<Integer>& range2, BinaryPredicate pred) {
+		const Integer
+			b(std::max(*range1.begin(), *range2.begin(), pred)),
+			e(std::min(*range1.end(), *range2.end(), pred));
+		return boost::irange(b, std::max(b, e, pred));
+	}
 #endif	// ASCENSION_ABANDONED_AT_VERSION_08
 
 #ifdef ASCENSION_ABANDONED_AT_VERSION_08
@@ -176,13 +225,21 @@ namespace ascension {
 	 * @param range The range
 	 * @param other The other range to test
 	 * @return true if @a range intersects with @a other
-	 * @see intersected
+	 * @see intersection
 	 */
 	template<typename T, typename Comp, typename Other>
 	inline bool intersects(const Range<T, Comp>& range, const Range<Other, Comp>& other) {
-		return !isEmpty(intersected(range, other));
+		return !isEmpty(intersection(range, other));
 	}
 #else
+	template<typename RandomAccessTraversalRange>
+	inline bool intersects(const RandomAccessTraversalRange& range1, const RandomAccessTraversalRange& range2) {
+		return boost::empty(intersection(range1, range2));
+	}
+	template<typename RandomAccessTraversalRange, typename BinaryPredicate>
+	inline bool intersects(const RandomAccessTraversalRange& range1, const RandomAccessTraversalRange& range2, BinaryPredicate pred) {
+		return boost::empty(intersection(range1, range2, pred));
+	}
 #endif	// ASCENSION_ABANDONED_AT_VERSION_08
 
 #ifdef ASCENSION_ABANDONED_AT_VERSION_08
@@ -198,7 +255,6 @@ namespace ascension {
 			typename std::tr1::enable_if<std::is_same<Comp, std::less<T>>::value>::type* = nullptr) {
 		return range.beginning() == range.end();
 	}
-#else
 #endif	// ASCENSION_ABANDONED_AT_VERSION_08
 
 #ifdef ASCENSION_ABANDONED_AT_VERSION_08
@@ -215,7 +271,6 @@ namespace ascension {
 		const Comp lessThan;
 		return !lessThan(range.beginning(), range.end()) && !lessThan(range.end(), range.beginning());
 	}
-#else
 #endif	// ASCENSION_ABANDONED_AT_VERSION_08
 
 #ifdef ASCENSION_ABANDONED_AT_VERSION_08
@@ -226,12 +281,11 @@ namespace ascension {
 	 * @param range The range to test
 	 * @return The length of @a range
 	 * @note This class does not define a method named "size".
-	 */
+	 */s
 	template<typename T, typename Comp>
 	inline typename detail::DifferenceType<T>::Type length(const Range<T, Comp>& range) {
 		return range.end() - range.beginning();
 	}
-#else
 #endif	// ASCENSION_ABANDONED_AT_VERSION_08
 
 #ifdef ASCENSION_ABANDONED_AT_VERSION_08
@@ -273,6 +327,18 @@ namespace ascension {
 	}
 #else
 #endif	// ASCENSION_ABANDONED_AT_VERSION_08
+
+	template<typename RandomAccessTraversalRange>
+	inline boost::iterator_range<typename boost::range_iterator<RandomAccessTraversalRange>::type> ordered(const RandomAccessTraversalRange& range) {
+		const auto temp = std::minmax(*boost::const_begin(range), *boost::const_end(range));
+		return boost::make_iterator_range<typename boost::range_iterator<RandomAccessTraversalRange>::type>(temp);
+	}
+
+	template<typename Integer>
+	inline boost::integer_range<Integer> ordered(const boost::integer_range<Integer>& range) {
+		const auto temp = std::minmax(*range.begin(), *range.end());
+		return boost::irange(temp.first, temp.second);
+	}
 
 #ifdef ASCENSION_ABANDONED_AT_VERSION_08
 	/**
