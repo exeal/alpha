c2184743c7d9fdbedd23c1ca83bf242632224935
exeal <luigi.irmr@gmail.com>
2014-01-27 17:07:54 +0900

    Replaced viewers.CaretStateListener interface with simple corresponding signals.

--- ascension/ascension/viewer/caret-observers.hpp
+++ ascension/ascension/viewer/caret-observers.hpp
@@ -23,27 +23,6 @@ namespace ascension {
 		class Caret;
 
 		/**
-		 * Interface for objects which are interested in getting informed about changes of a caret.
-		 * @see IPointListener, Caret#addStateListener, Caret#removeStateListener
-		 */
-		class CaretStateListener {
-		private:
-			/**
-			 * The matched brackets were changed.
-			 * @param caret The caret
-			 * @param oldPair The pair of the brackets previously matched. May be @c boost#none
-			 * @param outsideOfView The brackets newly matched are outside of the view
-			 */
-			virtual void matchBracketsChanged(const Caret& caret,
-				const boost::optional<std::pair<kernel::Position, kernel::Position>>& oldPair, bool outsideOfView) = 0;
-			/// The overtype mode of the caret is changed.
-			virtual void overtypeModeChanged(const Caret& caret) = 0;
-			/// The shape (linear or rectangle) of the selection is changed.
-			virtual void selectionShapeChanged(const Caret& caret) = 0;
-			friend class Caret;
-		};
-
-		/**
 		 * Interface for objects which are interested in change of input's properties of a
 		 * @c TextViewer.
 		 * @see CaretListener#overtypeModeChanged, TextViewer#addInputPropertyListener,
