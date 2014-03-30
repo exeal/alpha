40341e705e6cdfde682208fde15424a4b17cdf0e
exeal <luigi.irmr@gmail.com>
2014-03-30 22:05:31 +0900

    Added explicit lock/unlock methods to LockGuard class.

--- ascension/ascension/corelib/basic-types.hpp
+++ ascension/ascension/corelib/basic-types.hpp
@@ -58,16 +58,28 @@ namespace ascension {
 		struct NullDeleter {
 			void operator()(const void*) BOOST_NOEXCEPT {}
 		};
-		template<typename Lockable, void(Lockable::*lock)(void), void(Lockable::*unlock)(void)>
+		template<typename Lockable, void(Lockable::*_lock)(void), void(Lockable::*_unlock)(void)>
 		class LockGuard : private boost::noncopyable {
 		public:
-			explicit LockGuard(Lockable* lockable) : lockable_(lockable) {
-				if(lockable_ != nullptr)
-					(lockable_->*lock)();
+			explicit LockGuard(Lockable* lockable) : lockable_(nullptr) {
+				if(lockable != nullptr)
+					lock(*lockable);
 			}
 			~LockGuard() {
-				if(lockable_ != nullptr)
-					(lockable_->*unlock)();
+				unlock();
+			}
+			void lock(Lockable& lockable) {
+				if(&lockable != lockable_) {
+					unlock();
+					(lockable.*_lock)();
+					lockable_ = &lockable;
+				}
+			}
+			void unlock() {
+				if(lockable_ != nullptr) {
+					(lockable_->*_unlock)();
+					lockable_ = nullptr;
+				}
 			}
 		private:
 			Lockable* lockable_;
