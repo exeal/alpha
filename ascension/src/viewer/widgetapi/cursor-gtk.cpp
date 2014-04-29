befcbff1d8f736064563c95adb9cdb5c73974097
exeal <luigi.irmr@gmail.com>
2014-04-29 17:38:54 +0900

    Implemented Cursor.createMonochrome and Cursor.position methods in gtkmm.

--- ascension/src/viewer/widgetapi/cursor-gtk.cpp
+++ ascension/src/viewer/widgetapi/cursor-gtk.cpp
@@ -7,6 +7,8 @@
 #include <ascension/viewer/widgetapi/cursor.hpp>
 #if ASCENSION_SELECTS_WINDOW_SYSTEM(GTK)
 #include <ascension/graphics/image.hpp>
+#include <gdkmm/device.h>
+#include <gdkmm/devicemanager.h>
 #if ASCENSION_SUPPORTS_GRAPHICS_SYSTEM(WIN32_GDI)
 #	include <ascension/graphics/rendering-context.hpp>
 #	include <cairomm/win32_surface.h>
@@ -55,6 +57,56 @@ namespace ascension {
 					impl_ = Gdk::Cursor::create(const_cast<Cursor&>(other).asNativeObject()->get_display(), other.impl_->get_image()->copy(), -1, -1);
 #endif
 				}
+			}
+
+			Cursor&& Cursor::createMonochrome(
+					const graphics::geometry::BasicDimension<Cursor::Coordinate>& size,
+					const std::uint8_t* bitmap, const std::uint8_t* mask,
+					const boost::optional<graphics::geometry::BasicPoint<Cursor::Coordinate>>& hotspot /* = boost::none */) {
+				if(bitmap == nullptr)
+					throw NullPointerException("bitmap");
+				else if(mask == nullptr)
+					throw NullPointerException("mask");
+
+				const std::uint32_t stride = graphics::Image::stride(graphics::geometry::dx(size), graphics::Image::ARGB32);
+				const std::uint32_t dy = graphics::geometry::dy(size);
+				std::unique_ptr<std::uint8_t[]> bits(new std::uint8_t[stride * dy]);
+
+				for(std::size_t x = 0, byteOffset = 0, bitOffset = 0; x < stride; x += 4) {
+					for(std::size_t y = 0; y < dy; ++y) {
+						std::uint32_t& pixel = *reinterpret_cast<std::uint32_t*>(bits[y * stride + x]);
+						const std::uint8_t bitMask = 1 << bitOffset;
+						pixel = ((bitmap[byteOffset] & bitMask) != 0) ? 0 : 0xffffffu;
+						pixel |= ((mask[byteOffset] & bitMask) != 0) ? 0 : 0xff000000u;
+
+						if(++bitOffset == 8) {
+							bitOffset = 0;
+							++byteOffset;
+						}
+					}
+				}
+				return Cursor(graphics::Image(std::move(bits), size, graphics::Image::ARGB32), hotspot);
+			}
+
+			void Cursor::hide() {
+				// TODO: Not implemented.
+			}
+
+			graphics::Point Cursor::position() {
+				if(const Glib::RefPtr<const Gdk::Display> defaultDisplay = Gdk::Display::get_default()) {
+					if(const Glib::RefPtr<const Gdk::DeviceManager> deviceManager = defaultDisplay->get_device_manager()) {
+						if(const Glib::RefPtr<const Gdk::Device> clientPointer = deviceManager->get_client_pointer()) {
+							int x, y;
+							clientPointer->get_position(x, y);
+							return graphics::geometry::make<graphics::Point>((graphics::geometry::_x = x, graphics::geometry::_y = y));
+						}
+					}
+				}
+				throw makePlatformError();
+			}
+
+			void Cursor::show() {
+				// TODO: Not implemented.
 			}
 		}
 	}
