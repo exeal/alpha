3d0936a4ab5bb6990261488c45cfc30b05a267dd
exeal <luigi.irmr@gmail.com>
2014-10-10 23:08:03 +0900

    .

--- ascension/ascension/presentation/text-run-style.hpp
+++ ascension/ascension/presentation/text-run-style.hpp
@@ -11,6 +11,9 @@
 
 #ifndef ASCENSION_TEXT_RUN_STYLE_HPP
 #define ASCENSION_TEXT_RUN_STYLE_HPP
+#ifndef FUSION_MAX_VECTOR_SIZE
+#	define FUSION_MAX_VECTOR_SIZE 30
+#endif
 
 #include <ascension/directions.hpp>
 #include <ascension/presentation/styles/auxiliary.hpp>
@@ -21,6 +24,10 @@
 #include <ascension/presentation/styles/inline.hpp>
 #include <ascension/presentation/styles/text.hpp>
 #include <ascension/presentation/styles/text-decor.hpp>
+#include <boost/fusion/algorithm/transformation/transform.hpp>
+#include <boost/fusion/container/vector.hpp>
+#include <boost/mpl/placeholders.hpp>
+#include <boost/mpl/transform.hpp>
 #include <memory>
 #include <boost/range/irange.hpp>
 
@@ -30,72 +37,49 @@ namespace ascension {
 		 * Declares visual style settings of a text run.
 		 * @see TextLineStyle, TextToplevelStyle, StyledTextRunIterator
 		 */
-		struct TextRunStyle :
-				public FastArenaObject<TextRunStyle>,
-				public std::enable_shared_from_this<TextRunStyle> {
-			/// @name Colors
-			/// @{
+		typedef boost::fusion::vector<
+			// Colors
 #if 1
-			styles::Color<styles::Inherited<true>> color;	///< The 'color' property.
+			styles::Color<styles::Inherited<true>>,		// 'color' property
 #else
-			std::shared_ptr<graphics::Paint> foreground;	///< Text paint style.
+			std::shared_ptr<graphics::Paint>,			// text paint style
 #endif
-			/// @}
+			// Backgrounds and Borders
+			styles::Background,							// 'background properties
+			styles::Border,								// 'border' properties
+			// Basic Box Model
+			FlowRelativeFourSides<styles::PaddingSide>,	// 'padding' properties			
+			FlowRelativeFourSides<styles::MarginSide>,	// 'margin' properties
+			// Fonts
+			styles::FontFamily,							// 'font-family' property
+			styles::FontWeight,							// 'font-weight' property
+			styles::FontStretch,						// 'font-stretch' property
+			styles::FontStyle,							// 'font-style' property
+			styles::FontSize,							// 'font-size property
+			styles::FontSizeAdjust,						// 'font-size-adjust' property
+//			styles::FontFeatureSettings,				// 'font-feature-settings' property
+//			styles::FontLanguageOverride,				// 'font-language-override' property
+			// Inline Layout
+			styles::TextHeight,							// 'text-height' property
+			styles::LineHeight,							// 'line-height' property
+			styles::DominantBaseline,					// 'dominant-baseline' property
+			styles::AlignmentBaseline,					// 'alignment-baseline' property
+			styles::AlignmentAdjust,					// 'alignment-adjust' property
+			styles::BaselineShift,						// 'baseline-shift' property
+			// Text
+			styles::TextTransform,						// 'text-transform' property
+			styles::Hyphens,							// 'hyphens' property
+			styles::WordSpacing,						// 'word-spacing' property
+			styles::LetterSpacing,						// 'letter-spacing' property
+			// Text Decoration
+			styles::TextDecoration,						// 'text-decoration' properties
+			styles::TextEmphasis,						// 'text-emphasis' properties
+			styles::TextShadow,							// 'text-shadow' property
+			// Auxiliary
+			styles::ShapingEnabled						// 'shaping-enabled' property
+		> TextRunStyle;
 
-			/// @name Backgrounds and Borders
-			/// @{
-			styles::Background background;	///< The 'background properties.
-			styles::Border border;			///< The 'border' properties.
-			/// @}
-
-			/// @name Basic Box Model
-			/// @{			
-			FlowRelativeFourSides<styles::PaddingSide> padding;	///< The 'padding' properties.			
-			FlowRelativeFourSides<styles::MarginSide> margin;	///< The 'margin' properties.
-			/// @}
-
-			/// @name Fonts
-			/// @{
-			styles::FontFamily fontFamily;						///< The 'font-family' property.
-			styles::FontWeight fontWeight;						///< The 'font-weight' property.
-			styles::FontStretch fontStretch;					///< The 'font-stretch' property.
-			styles::FontStyle fontStyle;						///< The 'font-style' property.
-			styles::FontSize fontSize;							///< The 'font-size property.
-			styles::FontSizeAdjust fontSizeAdjust;				///< The 'font-size-adjust' property.
-//			styles::FontFeatureSettings fontFeatureSettings;	///< The 'font-feature-settings' property.
-//			styles::FontLanguageOverride fontLanguageOverride;	///< The 'font-language-override' property.
-			/// @}
-
-			/// @name Inline Layout
-			/// @{
-			styles::TextHeight textHeight;					///< The 'text-height' property.
-			styles::LineHeight lineHeight;					///< The 'line-height' property.
-			styles::DominantBaseline dominantBaseline;		///< The 'dominant-baseline' property.
-			styles::AlignmentBaseline alignmentBaseline;	///< The 'alignment-baseline' property.
-			styles::AlignmentAdjust alignmentAdjust;		///< The 'alignment-adjust' property.
-			styles::BaselineShift baselineShift;			///< The 'baseline-shift' property.
-			/// @}
-
-			/// @name Text
-			/// @{
-			styles::TextTransform textTransform;	///< The 'text-transform' property.
-			styles::Hyphens hyphens;				///< The 'hyphens' property.
-			styles::WordSpacing wordSpacing;		///< The 'word-spacing' property.
-			styles::LetterSpacing letterSpacing;	///< The 'letter-spacing' property.
-			/// @}
-
-			/// @name Text Decoration
-			/// @{
-			styles::TextDecoration textDecoration;	///< The 'text-decoration' properties.
-			styles::TextEmphasis textEmphasis;		///< The 'text-emphasis' properties.
-			styles::TextShadow textShadow;			///< The 'text-shadow' property.
-			/// @}
-
-			/// @name Auxiliary
-			/// @{
-			styles::ShapingEnabled shapingEnabled;	///< The 'shaping-enabled' property.
-			/// @}
-		};
+		// TODO: Check uniqueness of the members of TextRunStyle.
 #if 0
 		/**
 		 * A @c StyledTextRun represents a text range with declared style. @c #beginning and
@@ -153,6 +137,16 @@ namespace ascension {
 			 */
 			virtual void next() = 0;
 		};
+
+		typedef boost::fusion::result_of::as_vector<
+			boost::mpl::transform<TextRunStyle, styles::SpecifiedValueType<boost::mpl::_1>>::type
+		>::type SpecifiedTextRunStyle;
+		typedef boost::fusion::result_of::as_vector<
+			boost::mpl::transform<TextRunStyle, styles::ComputedValueType<boost::mpl::_1>>::type
+		>::type ComputedTextRunStyle;
+
+		void computeTextRunStyle(const SpecifiedTextRunStyle& specifiedValues,
+			const styles::Length::Context& context, ComputedTextRunStyle& computedValues);
 	}
 } // namespace ascension.presentation
 
