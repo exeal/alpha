de1a1c436967749f9e3f82de08bfcf416cb65fda
exeal <exeal@users.sourceforge.jp>
2014-10-13 19:45:48 +0900

    Separated TextPaintOverride class into new text-paint-override.hpp header file.

--- ascension/ascension/graphics/font/text-layout-styles.hpp
+++ ascension/ascension/graphics/font/text-layout-styles.hpp
@@ -12,7 +12,6 @@
 #define ASCENSION_TEXT_LAYOUT_STYLES_HPP
 
 #include <ascension/graphics/font/computed-text-styles.hpp>
-#include <vector>
 
 namespace ascension {
 	namespace graphics {
@@ -20,39 +19,6 @@ namespace ascension {
 		class Paint;
 
 		namespace font {
-			class TextPaintOverride {
-			public:
-				struct Segment {
-					/// The length of this segment.
-					Index length;
-					/// The overridden foreground or @c null if does not override.
-					std::shared_ptr<const Paint> foreground;
-					/// The transparency of the overridden foreground. This value should be in the
-					/// range from 0.0 (fully transparent) to 1.0 (no additional transparency).
-					double foregroundAlpha;
-					/// The overridden background or @c null if does not override.
-					std::shared_ptr<const Paint> background;
-					/// The transparency of the overridden background. This value should be in the
-					/// range from 0.0 (fully transparent) to 1.0 (no additional transparency).
-					double backgroundAlpha;
-					/// Set @c false to paint only the glyphs' bounds with @c #background.
-					/// Otherwise the logical highlight bounds of characters are painted as
-					/// background.
-					bool usesLogicalHighlightBounds;
-				};
-			public:
-				/// Destructor.
-				virtual ~TextPaintOverride() BOOST_NOEXCEPT {}
-				/**
-				 * Returns a vector of segments which describe override the paints of the specified
-				 * character range in the line.
-				 * @param range The character range in the line
-				 * @return A vector of @c #Segment
-				 */
-				virtual std::vector<const Segment>&&
-					queryTextPaintOverride(const boost::integer_range<Index>& range) const = 0;
-			};
-
 			/// Computed values of core properties of @c presentation#TextRunStyle.
 			struct ComputedTextRunStyleCore : private boost::equality_comparable<ComputedTextRunStyleCore> {
 				/// Computed value of @c TextRunStyle#color property.
