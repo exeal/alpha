0326e766d05f5f5dd9159804c4cc3269cc52cc7e
exeal <luigi.irmr@gmail.com>
2014-10-29 20:15:11 +0900

    Removed presentation.GlobalTextStyleSwitch interface.

--- ascension/ascension/presentation/text-toplevel-style.hpp
+++ ascension/ascension/presentation/text-toplevel-style.hpp
@@ -55,9 +55,8 @@ namespace ascension {
 
 		/// "Specified Values" of @c TextToplevelStyle.
 #if 1
-		struct SpecifiedTextToplevelStyle : boost::fusion::result_of::as_vector<
-			boost::mpl::transform<TextToplevelStyle, styles::SpecifiedValueType<boost::mpl::_1>>::type
-		>::type {};
+		struct SpecifiedTextToplevelStyle :
+			boost::mpl::transform<TextToplevelStyle, styles::SpecifiedValueType<boost::mpl::_1>>::type {};
 #else
 		typedef boost::fusion::result_of::as_vector<
 			boost::mpl::transform<TextToplevelStyle, styles::SpecifiedValueType<boost::mpl::_1>>::type
@@ -66,72 +65,16 @@ namespace ascension {
 
 		/// "Computed Values" of @c TextToplevelStyle.
 #if 1
-		struct ComputedTextToplevelStyle : boost::fusion::result_of::as_vector<
-			boost::mpl::transform<TextToplevelStyle, styles::ComputedValueType<boost::mpl::_1>>::type
-		>::type {};
+		struct ComputedTextToplevelStyle :
+			boost::mpl::transform<TextToplevelStyle, styles::ComputedValueType<boost::mpl::_1>>::type {};
 #else
 		typedef boost::fusion::result_of::as_vector<
 			boost::mpl::transform<TextToplevelStyle, styles::ComputedValueType<boost::mpl::_1>>::type
 		>::type ComputedTextToplevelStyle;
 #endif
 
-		std::size_t hash_value(const ComputedTextToplevelStyle& style);
-
-		/**
-		 * @see Presentation#computeTextLineStyle
-		 */
-		class GlobalTextStyleSwitch {
-		public:
-			/// Return type of @c #direction method.
-			typedef decltype(TextLineStyle().direction) Direction;
-			/// Return type of @c #textAlignment method.
-			typedef decltype(TextLineStyle().textAlignment) TextAlignment;
-			/// Return type of @c #textOrientation method.
-			typedef decltype(TextLineStyle().textOrientation) TextOrientation;
-			/// Return type of @c #whiteSpace method.
-			typedef decltype(TextLineStyle().whiteSpace) WhiteSpace;
-			/// Return type of @c #writingMode method.
-			typedef decltype(TextToplevelStyle().writingMode) WritingMode;
-		public:
-			/// Destructor.
-			virtual ~GlobalTextStyleSwitch() BOOST_NOEXCEPT {}
-
-		private:
-			/**
-			 * Returns the 'direction' style property which follows @c TextLineStyle#direction and
-			 * overrides @c TextToplevelStyle#defaultLineStyle#direction.
-			 * @return The declared value of 'direction' style property
-			 */
-			virtual Direction direction() const BOOST_NOEXCEPT = 0;
-			/**
-			 * Returns the 'text-align' style property which follows @c TextLineStyle#textAlignment
-			 * and overrides @c TextToplevelStyle#defaultLineStyle#textAlignment.
-			 * @return The declared value of 'text-align' style property
-			 */
-			virtual TextAlignment textAlignment() const BOOST_NOEXCEPT = 0;
-			/**
-			 * Returns 'text-orientation' style property which follows
-			 * @c TextLineStyle#textOrientation and overrides
-			 * @c TextToplevelStyle#defaultLineStyle#textOrientation.
-			 * @return The declared value of 'text-orientation' style property
-			 */
-			virtual TextOrientation textOrientation() const BOOST_NOEXCEPT = 0;
-			/**
-			 * Returns 'white-space' style property which follows @c TextLineStyle#whiteSpace and
-			 * overrides @c TextToplevelStyle#defaultLineStyle#whiteSpace.
-			 * @return The declared value of 'white-space' style property
-			 */
-			virtual WhiteSpace whiteSpace() const BOOST_NOEXCEPT = 0;
-			/**
-			 * Returns the 'writing-mode' style property which follows
-			 * @c TextToplevelStyle#writingMode.
-			 * @return The declared value of 'writing-mode' style property
-			 */
-			virtual WritingMode writingMode() const BOOST_NOEXCEPT = 0;
-			friend class Presentation;
-		};
-
 		boost::flyweight<ComputedTextToplevelStyle> compute(const SpecifiedTextToplevelStyle& specifiedValues);
+		std::size_t hash_value(const ComputedTextToplevelStyle& style);
 	}
 } // namespace ascension.presentation
 
