1395814030f65b34dc3f09780d18cad0d8723271
exeal <exeal@users.sourceforge.jp>
2014-11-16 21:06:21 +0900

    Added rules/uri-detector.* files define URIDetector class.

---
+++ ascension/ascension/rules/uri-detector.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file uri-detector.hpp
+ * @author exeal
+ * @date 2004-2006 was Lexer.h
+ * @date 2006-2014 was rules.hpp
+ * @date 2014-11-16 Separated from rules.hpp
+ */
+
+#ifndef ASCENSION_URI_DETECTOR_HPP
+#define ASCENSION_URI_DETECTOR_HPP
+
+#include <ascension/corelib/string-piece.hpp>
+#include <boost/core/noncopyable.hpp>
+#include <set>
+
+namespace ascension {
+	namespace rules {
+		namespace detail {
+			class HashTable;
+		}
+
+		/**
+		 * A @c URIDetector detects and searches URI. This class conforms to the syntaxes of
+		 * <a href="http://www.ietf.org/rfc/rfc3986.txt">RFC 3986</a> and
+		 * <a href="http://www.ietf.org/rfc/rfc3987.txt">RFC 3987</a>.
+		 */
+		class URIDetector : private boost::noncopyable {
+		public:
+			explicit URIDetector() BOOST_NOEXCEPT;
+			~URIDetector() BOOST_NOEXCEPT;
+
+			/// @name Parsing
+			/// @{
+			StringPiece::const_iterator detect(const StringPiece& text) const;
+			StringPiece search(const StringPiece& text) const;
+			/// @}
+
+			/// @name Attribute
+			/// @{
+			URIDetector& setValidSchemes(const std::set<String>& schemes, bool caseSensitive = false);
+			URIDetector& setValidSchemes(const String& schemes, Char separator, bool caseSensitive = false);
+			/// @}
+
+			/// @name Default Instances
+			/// @{
+			static const URIDetector& defaultGenericInstance() BOOST_NOEXCEPT;
+			static const URIDetector& defaultIANAURIInstance() BOOST_NOEXCEPT;
+			/// @}
+
+		private:
+			detail::HashTable* validSchemes_;
+		};
+	}
+} // namespace ascension.rules
+
+#endif // !ASCENSION_URI_DETECTOR_HPP
