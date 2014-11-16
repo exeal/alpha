069d8b18c71b6166eb37e9bfcef2112909704abb
exeal <exeal@users.sourceforge.jp>
2014-11-16 23:14:16 +0900

    Separated the definition of LexicalPartitioner class into the new lexical-partitioner.* files.

--- ascension/ascension/rules.hpp
+++ ascension/ascension/rules.hpp
@@ -104,52 +104,6 @@ namespace ascension {
 #endif // !ASCENSION_NO_REGEX
 
 		/**
-		 * @c LexicalPartitioner makes document partitions by using the specified lexical rules.
-		 * @note This class is not derivable.
-		 * @see kernel#Document
-		 */
-		class LexicalPartitioner : public kernel::DocumentPartitioner, private boost::noncopyable {
-		public:
-			// constructor
-			LexicalPartitioner() BOOST_NOEXCEPT;
-			~LexicalPartitioner() BOOST_NOEXCEPT;
-			// attribute
-			template<typename SinglePassReadableRange>
-			void setRules(const SinglePassReadableRange& rules);
-		private:
-			struct Partition {
-				kernel::ContentType contentType;
-				kernel::Position start, tokenStart;
-				Index tokenLength;
-				Partition(kernel::ContentType type, const kernel::Position& p,
-					const kernel::Position& startOfToken, Index lengthOfToken) BOOST_NOEXCEPT
-					: contentType(type), start(p), tokenStart(startOfToken), tokenLength(lengthOfToken) {}
-				kernel::Position getTokenEnd() const BOOST_NOEXCEPT {
-					return kernel::Position(tokenStart.line, tokenStart.offsetInLine + tokenLength);
-				}
-			};
-		private:
-			void computePartitioning(const kernel::Position& start,
-				const kernel::Position& minimalLast, kernel::Region& changedRegion);
-//			static void deleteRules(std::list<const TransitionRule*>& rules) BOOST_NOEXCEPT;
-			void dump() const;
-			void erasePartitions(const kernel::Position& first, const kernel::Position& last);
-			ascension::detail::GapVector<Partition*>::const_iterator partitionAt(const kernel::Position& at) const BOOST_NOEXCEPT;
-			kernel::ContentType transitionStateAt(const kernel::Position& at) const BOOST_NOEXCEPT;
-			Index tryTransition(const String& line, Index offsetInLine,
-				kernel::ContentType contentType, kernel::ContentType& destination) const BOOST_NOEXCEPT;
-			void verify() const;
-			// DocumentPartitioner
-			void documentAboutToBeChanged() BOOST_NOEXCEPT;
-			void documentChanged(const kernel::DocumentChange& change) BOOST_NOEXCEPT;
-			void doGetPartition(const kernel::Position& at, kernel::DocumentPartition& partition) const BOOST_NOEXCEPT;
-			void doInstall() BOOST_NOEXCEPT;
-		private:
-			ascension::detail::GapVector<Partition*> partitions_;
-			std::forward_list<std::unique_ptr<const TransitionRule>> rules_;
-		};
-
-		/**
 		 * Standard implementation of @c presentation#IPartitionPresentationReconstructor. This
 		 * implementation performs rule based lexical tokenization using the given @c TokenScanner.
 		 * @note This class is not intended to be subclassed.
@@ -170,24 +124,6 @@ namespace ascension {
 			std::shared_ptr<const presentation::TextRunStyle> defaultStyle_;
 			const std::map<Token::Identifier, std::shared_ptr<const presentation::TextRunStyle>> styles_;
 		};
-
-		/**
-		 * @tparam SinglePassReadableRange The type of @a rules
-		 * @param rules The new rules to set
-		 */
-		template<typename SinglePassReadableRange>
-		inline void LexicalPartitioner::setRules(const SinglePassReadableRange& rules) {
-			if(document() != nullptr)
-				throw IllegalStateException("The partitioner is already connected to document.");
-//			std::forward_list<std::unique_ptr<const TransitionRule>> newRules(
-//				std::make_move_iterator(std::begin(rules)), std::make_move_iterator(std::end(rules)));
-			std::forward_list<std::unique_ptr<const TransitionRule>> newRules;
-			boost::for_each(rules, [&newRules](const std::unique_ptr<const TransitionRule>& rule) mutable {
-				newRules.push_front(std::move(rule->clone()));
-			});
-//			std::swap(rules_, newRules);
-		}
-
 	}
 } // namespace ascension.rules
 
