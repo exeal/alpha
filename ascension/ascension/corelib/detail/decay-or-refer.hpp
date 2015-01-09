8a3d0a6ee839caf65ed33235b7263d15bfd3fde1
exeal <luigi.irmr@gmail.com>
2015-01-09 11:37:19 +0900

    Separated detail.DecayOrRefer internal class template from directions.hpp.

---
+++ ascension/ascension/corelib/detail/decay-or-refer.hpp
@@ -0,0 +1,26 @@
+/**
+ * @internal
+ * @file decay-or-refer
+ * @date 2012-03-31 created
+ * @date 2012-2014 was directions.hpp
+ * @date 2015-01-09 Separated from directions.hpp
+ */
+
+#ifndef ASCENSION_DECAY_OR_REFER_HPP
+#define ASCENSION_DECAY_OR_REFER_HPP
+
+#include <type_traits>
+#include <utility>
+
+namespace ascension {
+	namespace detail {
+		template<typename T> struct DecayOrRefer {
+			typedef typename std::decay<T>::type Type;
+		};
+		template<typename T> struct DecayOrRefer<std::reference_wrapper<T>> {
+			typedef T& Type;
+		};
+	}
+}
+
+#endif // !ASCENSION_DECAY_OR_REFER_HPP
