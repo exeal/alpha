fd1ce29d20b5f1b17f449dbabe43638a61485009
exeal <exeal@users.sourceforge.jp>
2015-05-30 12:41:26 +0900

    Added operator== and hash_value for graphics.font.ActualTextRunStyleCore class.

--- ascension/src/graphics/font/actual-text-styles.cpp
+++ ascension/src/graphics/font/actual-text-styles.cpp
@@ -13,9 +13,11 @@ namespace ascension {
 		namespace font {
 			namespace {
 				/***/
-				PhysicalFourSides<ActualBorderSide> makeActualBorderSides(
-						const presentation::styles::ComputedValuesOfParts<presentation::TextRunStyleParts::BackgroundsAndBorders>::type& computed,
-						const presentation::WritingMode& writingMode, const presentation::styles::Length::Context& context) {
+				presentation::FlowRelativeFourSides<ActualBorderSide> makeActualBorderSides(
+						const presentation::styles::ComputedValuesOfParts<
+							presentation::TextRunStyleParts::BackgroundsAndBorders
+						>::type& computed,
+						const presentation::styles::Length::Context& context) {
 					presentation::FlowRelativeFourSides<ActualBorderSide> abstractSides;
 					const auto& computedColors = boost::fusion::at_key<presentation::FlowRelativeFourSides<presentation::styles::BorderColor>>(computed);
 					const auto& computedStyles = boost::fusion::at_key<presentation::FlowRelativeFourSides<presentation::styles::BorderStyle>>(computed);
@@ -26,21 +28,20 @@ namespace ascension {
 						boost::fusion::at_key<presentation::styles::BorderWidth>(abstractSides[i]) = computedWidths[i].value(context);
 					}
 
-					return presentation::mapFlowRelativeToPhysical(writingMode, abstractSides);
+					return presentation::FlowRelativeFourSides<ActualBorderSide>(abstractSides);
 				}
 			}
 
 			/**
 			 * Creates @c ActualTextRunStyleCore object.
 			 * @param computed The "Computed Value" of the text run styles
-			 * @param writingMode The writing modes to map flow-relative bounds into physical
 			 * @param context The rendering context to map between pixels and user units
 			 */
-			ActualTextRunStyleCore::ActualTextRunStyleCore(const presentation::ComputedTextRunStyle& computed,
-					const presentation::WritingMode& writingMode, const presentation::styles::Length::Context& context) :
+			ActualTextRunStyleCore::ActualTextRunStyleCore(
+					const presentation::ComputedTextRunStyle& computed, const presentation::styles::Length::Context& context) :
 				color(boost::fusion::at_key<presentation::styles::Color>(computed.colors)),
 				backgroundColor(boost::fusion::at_key<presentation::styles::BackgroundColor>(computed.backgroundsAndBorders)),
-				borders(makeActualBorderSides(computed.backgroundsAndBorders, writingMode, context)),
+				borders(makeActualBorderSides(computed.backgroundsAndBorders, context)),
 				textDecoration(
 					boost::fusion::at_key<presentation::styles::TextDecorationLine>(computed.textDecoration),
 					boost::fusion::at_key<presentation::styles::TextDecorationColor>(computed.textDecoration),
