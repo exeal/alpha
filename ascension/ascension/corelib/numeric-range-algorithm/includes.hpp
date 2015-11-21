2cb6084f9afbbd13fb2ada61181754bd6570d0ec
exeal <luigi.irmr@gmail.com>
2015-11-21 13:13:56 +0900

    Simplified the parameter list of algorithm.includes free functions.

--- ascension/ascension/corelib/numeric-range-algorithm/includes.hpp
+++ ascension/ascension/corelib/numeric-range-algorithm/includes.hpp
@@ -10,29 +10,25 @@
 #include <boost/range/begin.hpp>
 #include <boost/range/end.hpp>
 #include <boost/range/value_type.hpp>
-#include <type_traits>
 
 namespace ascension {
 	namespace algorithm {
 		/**
 		 * Returns @c true if the numeric range includes the given value.
 		 * @tparam NumericRange The type of @a bounds
-		 * @tparam Value The type of @a value
 		 * @param bounds The numeric range to test
 		 * @param value The numeric value to test
 		 * @return @c true if @a value in [lo, hi)
 		 * @note This function assumes that @a bounds is ordered.
 		 */
-		template<typename NumericRange, typename Value>
-		inline bool includes(const NumericRange& bounds, Value value,
-				typename std::enable_if<std::is_convertible<Value, typename boost::range_value<NumericRange>::type>::value>::type* = nullptr) {
+		template<typename NumericRange>
+		inline bool includes(const NumericRange& bounds, typename boost::range_value<NumericRange>::type value) {
 			return value >= *boost::const_begin(bounds) && value < *boost::const_end(bounds);
 		}
 
 		/**
 		 * Returns @c true if the numeric range includes the given value.
 		 * @tparam NumericRange The type of @a bounds
-		 * @tparam Value The type of @a value
 		 * @tparam BinaryPredicate The type of @a pred
 		 * @param bounds The numeric range to test
 		 * @param value The numeric value to test
@@ -40,39 +36,34 @@ namespace ascension {
 		 * @return @c true if @a value in [lo, hi)
 		 * @note This function assumes that @a bounds is ordered.
 		 */
-		template<typename NumericRange, typename Value, typename BinaryPredicate>
-		inline bool includes(const NumericRange& bounds, Value value, BinaryPredicate pred,
-				typename std::enable_if<std::is_convertible<Value, typename boost::range_value<NumericRange>::type>::value>::type* = nullptr) {
+		template<typename NumericRange, typename BinaryPredicate>
+		inline bool includes(const NumericRange& bounds, typename boost::range_value<NumericRange>::type value, BinaryPredicate pred) {
 			return !pred(value, *boost::const_begin(bounds)) && pred(value, *boost::const_end(bounds));
 		}
 
 		/**
 		 * Returns @c true if the numeric range includes the other numeric range.
-		 * @tparam NumericRange1 The type of @a bounds
-		 * @tparam NumericRange2 The type of @a range
+		 * @tparam NumericRange The type of @a bounds and @a range
 		 * @param bounds The numeric range to test
 		 * @param range The numeric range to test
 		 * @return @c true if @a bounds includes @a range
 		 * @note This function assumes that both @a bounds and @a range are ordered.
 		 */
-		template<typename NumericRange1, typename NumericRange2>
-		inline bool includes(const NumericRange1& bounds, const NumericRange2& range,
-				typename std::enable_if<!std::is_convertible<NumericRange2, typename boost::range_value<NumericRange1>::type>::value>::type* = nullptr) {
+		template<typename NumericRange>
+		inline bool includes(const NumericRange& bounds, const NumericRange& range) {
 			return *boost::const_begin(range) >= *boost::const_begin(bounds) && *boost::const_end(range) <= *boost::const_end(bounds);
 		}
 
 		/**
 		 * Returns @c true if the numeric range includes the other numeric range.
-		 * @tparam NumericRange1 The type of @a bounds
-		 * @tparam NumericRange2 The type of @a range
+		 * @tparam NumericRange The type of @a bounds and @a range
 		 * @param bounds The numeric range to test
 		 * @param range The numeric range to test
 		 * @return @c true if @a bounds includes @a range
 		 * @note This function assumes that both @a bounds and @a range are ordered.
 		 */
-		template<typename NumericRange1, typename NumericRange2, typename BinaryPredicate>
-		inline bool includes(const NumericRange1& bounds, const NumericRange2& range, BinaryPredicate pred,
-				typename std::enable_if<!std::is_convertible<NumericRange2, typename boost::range_value<NumericRange1>::type>::value>::type* = nullptr) {
+		template<typename NumericRange, typename BinaryPredicate>
+		inline bool includes(const NumericRange& bounds, const NumericRange& range, BinaryPredicate pred) {
 			return !pred(*boost::const_begin(range), *boost::const_begin(bounds)) && !pred(*boost::const_end(range), *boost::const_end(bounds));
 		}
 	}
