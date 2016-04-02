5400465e09b5b8cc0fc80b6e1a19f54f475bde84
exeal <luigi.irmr@gmail.com>
2016-04-02 09:57:17 +0900

    Added overloads of graphics.geometry.translate function.

--- ascension/ascension/graphics/geometry/algorithms/translate.hpp
+++ ascension/ascension/graphics/geometry/algorithms/translate.hpp
@@ -16,32 +16,72 @@ namespace ascension {
 		namespace geometry {
 			/**
 			 * Calls @c boost#geometry#transform with @c boost#geometry#strategy#transform#translate_transformer.
-			 * @tparam Arguments The type of @a arguments
-			 * @param arguments The named parameters
+			 * @param from The source geometry
+			 * @param to The destination geometry
+			 * @param tx The x coordinate of the translation, or 0
+			 * @param ty The y coordinate of the translation, or 0
 			 * @return The returned value of @c boost#geometry#transform
 			 */
-			template<typename Arguments>
-			inline bool translate(const Arguments& arguments) {
+#ifndef ASCENSION_DOXYGEN_SHOULD_SKIP_THIS
+			BOOST_PARAMETER_FUNCTION(
+				(bool), translate, tag,
+				(required
+					(from, *)
+					(out(to), *))
+				(optional
+					(tx, *, 0)
+					(ty, *, 0))) {
 				return boost::geometry::transform(
-					arguments[_from], arguments[_to],
+					from, to,
 					boost::geometry::strategy::transform::translate_transformer<
-						boost::geometry::coordinate_type<std::decay<decltype(arguments[_from])>::type>::type,
-						boost::geometry::dimension<decltype(arguments[_from])>::value,
-						boost::geometry::dimension<decltype(arguments[_to])>::value
-					>(arguments[_dx], arguments[_dy]));
+						boost::geometry::coordinate_type<std::decay<decltype(from)>::type>::type,
+						boost::geometry::dimension<decltype(from)>::value,
+						boost::geometry::dimension<decltype(to)>::value
+					>(tx, ty));
+			}
+#else
+			template<typename From, typename To, typename Tx, typename Ty>
+			bool translate(const From& from, To& to, Tx tx, Ty ty);
+#endif
+
+			/**
+			 * Calls @c boost#geometry#transform with @c boost#geometry#strategy#transform#translate_transformer.
+			 * @tparam Geometry The type of @a g
+			 * @tparam Delta The type of @a delta
+			 * @param g The geometry to translate
+			 * @param delta The distance to translate
+			 * @return The returned value of @c boost#geometry#transform
+			 */
+			template<typename Geometry, typename Delta>
+			inline bool translate(Geometry& g, const Delta& delta, typename detail::EnableIfTagIs<Delta, DimensionTag>::type* = nullptr) {
+				return translate(_from = g, _to = g, _tx = dx(delta), _ty = dy(delta));
 			}
 
 			/**
 			 * Calls @c boost#geometry#transform with @c boost#geometry#strategy#transform#translate_transformer.
-			 * @tparam Arguments The type of @a arguments
+			 * @tparam Geometry The type of @a g
 			 * @tparam Delta The type of @a delta
-			 * @param arguments The named parameters
+			 * @param g The geometry to translate
+			 * @param delta The named parameters. 'tx' : The x coordinate of the translation. 'ty' : The y coordinate
+			 *              of the translation
+			 * @return The returned value of @c boost#geometry#transform
+			 */
+			template<typename Geometry, typename Delta>
+			inline bool translate(Geometry& g, const Delta& delta, typename detail::DisableIfTagIs<Delta, DimensionTag>::type* = nullptr) {
+				return translate(_from = g, _to = g, _tx = delta[_tx | 0], _ty = delta[_ty | 0]);
+			}
+
+			/**
+			 * Calls @c boost#geometry#transform with @c boost#geometry#strategy#transform#translate_transformer.
+			 * @tparam Geometries The type of @a geometries
+			 * @tparam Delta The type of @a delta
+			 * @param geometries The named parameters. 'from' : The source geometry. 'to' : The destination geometry
 			 * @param delta The distance to translate
 			 * @return The returned value of @c boost#geometry#transform
 			 */
-			template<typename Arguments, typename Delta>
-			inline bool translate(const Arguments& arguments, const Delta& delta, typename detail::EnableIfTagIs<Delta, DimensionTag>::type* = nullptr) {
-				return translate((_from = arguments[_from], _to = arguments[_to], _dx = dx(delta), _dy = dy(delta)));
+			template<typename Geometries, typename Delta>
+			inline bool translate(const Geometries& geometries, const Delta& delta, typename detail::EnableIfTagIs<Delta, DimensionTag>::type* = nullptr) {
+				return translate(_from = geometries[_from], _to = geometries[_to], _tx = dx(delta), _ty = dy(delta));
 			}
 		}
 	}
