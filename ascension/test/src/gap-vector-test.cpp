0490071828ee0baf4216729125362cddb81edc14
exeal <luigi.irmr@gmail.com>
2016-06-20 01:41:26 +0900

    Redesigned test project.

---
+++ ascension/test/src/gap-vector-test.cpp
@@ -0,0 +1,256 @@
+#define BOOST_TEST_MODULE gap_vector_test
+#include <boost/test/included/unit_test.hpp>
+
+#include <ascension/corelib/detail/gap-vector.hpp>
+#include <array>
+#include <cstdint>
+#include <iomanip>
+#include <string>
+
+namespace {
+	std::array<int, 8> make_random_array() {
+		const std::array<int, 8> a = {12, 23, 34, 45, 56, 67, 78, 89};
+		return a;
+	}
+	ascension::detail::GapVector<int> make_random_gap_vector() {
+		const auto a(make_random_array());
+		return ascension::detail::GapVector<int>(a.cbegin(), a.cend());
+	}
+	std::string to_string(const ascension::detail::GapVector<char>& gv) {
+		return std::string(gv.cbegin(), gv.cend());
+	}
+}
+
+namespace ascension {
+	namespace detail {
+		template<typename T, typename P, typename R>
+		inline std::ostream& operator<<(std::ostream& out, ascension::detail::GapVectorIterator<T, P, R> i) {
+			return out << "iterator{@" << std::hex << static_cast<unsigned int>(reinterpret_cast<std::uintptr_t>(&*i)) << "}";
+		}
+		template<typename T, typename P, typename R>
+		inline std::ostream& operator<<(std::ostream& out, std::reverse_iterator<ascension::detail::GapVectorIterator<T, P, R>> i) {
+			return out << "iterator{@" << std::hex << static_cast<unsigned int>(reinterpret_cast<std::uintptr_t>(&*i)) << "}";
+		}
+		template<typename T, typename A>
+		inline std::ostream& operator<<(std::ostream& out, const ascension::detail::GapVector<T, A>& gv) {
+			out << "gap_vector[";
+			std::copy(gv.cbegin(), gv.cend(), std::ostream_iterator<char>(out));
+			return out << "]";
+		}
+	}
+}
+
+BOOST_AUTO_TEST_SUITE(construction)
+	BOOST_AUTO_TEST_CASE(default_construction) {
+		const ascension::detail::GapVector<int> gv;
+		BOOST_TEST(gv.empty());
+		BOOST_TEST(gv.size() == static_cast<std::size_t>(0));
+		BOOST_TEST(gv.begin() == gv.end());
+		BOOST_TEST(gv.cbegin() == gv.cend());
+		BOOST_TEST(gv.rbegin() == gv.rend());
+		BOOST_TEST(gv.crbegin() == gv.crend());
+	}
+
+	BOOST_AUTO_TEST_CASE(nv_construction) {
+		const ascension::detail::GapVector<int> gv(static_cast<std::size_t>(3), 42);
+		BOOST_TEST(!gv.empty());
+		BOOST_TEST(gv.size() == static_cast<std::size_t>(3));
+		BOOST_TEST(gv.at(0) == 42);
+		BOOST_TEST(gv.at(1) == 42);
+		BOOST_TEST(gv.at(2) == 42);
+	}
+
+	BOOST_AUTO_TEST_CASE(iterators_construction) {
+		const std::array<int, 8> a(make_random_array());
+		const ascension::detail::GapVector<int> gv(a.cbegin(), a.cend());
+		BOOST_TEST(!gv.empty());
+		BOOST_TEST(gv.size() == a.size());
+		BOOST_CHECK_EQUAL_COLLECTIONS(gv.cbegin(), gv.cend(), a.cbegin(), a.cend());
+	}
+
+	BOOST_AUTO_TEST_CASE(copy_construction) {
+		const auto gv(make_random_gap_vector());
+		const auto gv2(gv);
+		BOOST_TEST(!gv2.empty());
+		BOOST_TEST(gv2.size() == gv.size());
+		BOOST_CHECK_EQUAL_COLLECTIONS(gv2.cbegin(), gv2.cend(), gv.cbegin(), gv.cend());
+	}
+
+	BOOST_AUTO_TEST_CASE(move_construction) {
+		const std::array<int, 8> a(make_random_array());
+		ascension::detail::GapVector<int> gv(a.cbegin(), a.cend());
+		const ascension::detail::GapVector<int> gv2(std::move(gv));
+		BOOST_TEST(!gv2.empty());
+		BOOST_TEST(gv2.size() == a.size());
+		BOOST_CHECK_EQUAL_COLLECTIONS(gv2.cbegin(), gv2.cend(), a.cbegin(), a.cend());
+	}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(assignment)
+	BOOST_AUTO_TEST_CASE(copy_assignment_operator_test) {
+		ascension::detail::GapVector<int> gv(make_random_gap_vector()), gv2;
+		auto& r = (gv2 = gv);
+		BOOST_TEST(gv2.size() == gv.size());
+		BOOST_CHECK_EQUAL_COLLECTIONS(gv2.cbegin(), gv2.cend(), gv.cbegin(), gv.cend());
+		BOOST_TEST(&r == &gv2);
+	}
+
+	BOOST_AUTO_TEST_CASE(move_assignment_operator_test) {
+		const auto a(make_random_array());
+		ascension::detail::GapVector<int> gv(a.cbegin(), a.cend()), gv2;
+		auto& r = (gv2 = std::move(gv));
+		BOOST_TEST(gv2.size() == a.size());
+		BOOST_CHECK_EQUAL_COLLECTIONS(gv2.cbegin(), gv2.cend(), a.cbegin(), a.cend());
+		BOOST_TEST(&r == &gv2);
+	}
+
+	BOOST_AUTO_TEST_CASE(assign_test) {
+		const auto a(make_random_array());
+		ascension::detail::GapVector<int> gv;
+		gv.assign(a.cbegin(), a.cend());
+		BOOST_TEST(gv.size() == a.size());
+		BOOST_CHECK_EQUAL_COLLECTIONS(gv.cbegin(), gv.cend(), a.cbegin(), a.cend());
+
+		gv.assign(static_cast<std::size_t>(3), 42);
+		BOOST_TEST(gv.size() == static_cast<std::size_t>(3));
+		BOOST_TEST(gv.at(0) == 42);
+		BOOST_TEST(gv.at(1) == 42);
+		BOOST_TEST(gv.at(2) == 42);
+	}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(element_access)
+	BOOST_AUTO_TEST_CASE(at_test) {
+		ascension::detail::GapVector<int> gv;
+		BOOST_CHECK_EXCEPTION(gv.at(0), std::out_of_range, [](const std::out_of_range&) {return true;});
+	}
+
+	BOOST_AUTO_TEST_CASE(front_back_test) {
+		auto gv(make_random_gap_vector());
+		BOOST_TEST(gv.front() == make_random_array().front());
+		BOOST_TEST(gv.back() == make_random_array().back());
+		BOOST_TEST(&gv.front() == &gv.front());
+		BOOST_TEST(&gv.back() == &gv.back());
+
+		gv.assign(static_cast<std::size_t>(1), 42);
+		BOOST_TEST(&gv.front() == &gv.back());
+	}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_CASE(iterators_test) {
+	ascension::detail::GapVector<int> gv, gv2;
+	BOOST_TEST(gv.begin() == gv.end());
+	BOOST_TEST(gv.cbegin() == gv.cend());
+	BOOST_TEST(gv.begin() != gv2.begin());
+
+	gv = make_random_gap_vector();
+	auto i(gv.cbegin());
+	BOOST_TEST(i < gv.cend());
+
+	BOOST_TEST(std::next(i, gv.size()) == gv.cend());
+	std::advance(i, gv.size());
+	BOOST_TEST(i == gv.cend());
+	BOOST_TEST(i > gv.cbegin());
+
+	BOOST_TEST(std::prev(i, gv.size()) == gv.cbegin());
+	std::advance(i, -static_cast<std::ptrdiff_t>(gv.size()));
+	BOOST_TEST(i == gv.cbegin());
+	BOOST_TEST(static_cast<std::size_t>(std::distance(i, gv.cend())) == gv.size());
+
+	i = gv.cend();
+	BOOST_TEST(i == gv.cend());
+
+	auto a(make_random_array());
+	BOOST_CHECK_EQUAL_COLLECTIONS(gv.rbegin(), gv.rend(), a.rbegin(), a.rend());
+	BOOST_CHECK_EQUAL_COLLECTIONS(gv.crbegin(), gv.crend(), a.crbegin(), a.crend());
+
+	for(auto i(gv.begin()); i != gv.end(); ++i)
+		*i *= 2;
+	for(auto i(a.begin()); i != a.end(); ++i)
+		*i *= 2;
+	BOOST_CHECK_EQUAL_COLLECTIONS(gv.cbegin(), gv.cend(), a.cbegin(), a.cend());
+}
+
+BOOST_AUTO_TEST_SUITE(modifications)
+	BOOST_AUTO_TEST_CASE(insert_test) {
+		ascension::detail::GapVector<char> gv;
+		char c = 'A';
+		auto i(gv.insert(gv.begin(), c));
+		BOOST_TEST(gv.size() == static_cast<std::size_t>(1));
+		BOOST_TEST(to_string(gv) == "A");
+		BOOST_TEST(i == gv.begin());
+
+		c = 'B';
+		i = gv.insert(gv.begin(), std::move(c));
+		BOOST_TEST(gv.size() == static_cast<std::size_t>(2));
+		BOOST_TEST(to_string(gv) == "BA");
+		BOOST_TEST(i == gv.begin());
+
+		i = gv.insert(gv.end(), 0, 'C');
+		BOOST_TEST(to_string(gv) == "BA");
+		BOOST_TEST(i == gv.end());
+
+		i = gv.insert(gv.end(), 3, 'D');
+		BOOST_TEST(to_string(gv) == "BADDD");
+		BOOST_TEST(i == std::prev(gv.end(), 3));
+
+		std::string s("EFG");
+		i = gv.insert(gv.begin() + 3, s.cbegin(), s.cend());
+		BOOST_TEST(to_string(gv) == "BADEFGDD");
+		BOOST_TEST(i == std::next(gv.begin(), 3));
+
+		s = "HIJ";
+		i = gv.insert(std::prev(gv.end(), 1), s.cbegin(), s.cend());
+		BOOST_TEST(to_string(gv) == "BADEFGDHIJD");
+		BOOST_TEST(i == std::prev(gv.end(), 4));
+	}
+
+	BOOST_AUTO_TEST_CASE(erase_test) {
+		const std::string source("ABCDEFGHIJKLMNOP");
+		ascension::detail::GapVector<char> gv(source.cbegin(), source.cend());
+		BOOST_REQUIRE_EQUAL_COLLECTIONS(gv.cbegin(), gv.cend(), source.cbegin(), source.cend());
+
+		auto i(gv.erase(gv.cbegin()));
+		BOOST_TEST(to_string(gv) == "BCDEFGHIJKLMNOP");
+		BOOST_TEST(i == gv.begin());
+
+		i = gv.erase(std::prev(gv.cend(), 1));
+		BOOST_TEST(to_string(gv) == "BCDEFGHIJKLMNO");
+		BOOST_TEST(i == gv.end());
+
+		i = gv.erase(std::next(gv.cbegin(), 3), std::prev(gv.cend(), 3));
+		BOOST_TEST(to_string(gv) == "BCDMNO");
+		BOOST_TEST(i == std::next(gv.begin(), 3));
+
+		i = gv.erase(gv.begin(), gv.end());
+		BOOST_TEST(gv.empty());
+		BOOST_TEST(i == gv.end());
+	}
+
+	BOOST_AUTO_TEST_CASE(clear_test) {
+		auto gv(make_random_gap_vector());
+		gv.clear();
+		BOOST_TEST(gv.empty());
+	}
+
+	BOOST_AUTO_TEST_CASE(comparisons_test) {
+		auto gv(make_random_gap_vector());
+		auto gv2(gv);
+		BOOST_TEST(gv == gv2);
+		BOOST_TEST(!(gv < gv2));
+
+		--gv.back();
+		BOOST_TEST(gv < gv2);
+
+		++gv.front();
+		BOOST_TEST(gv > gv2);
+	}
+
+	BOOST_AUTO_TEST_CASE(swap_test) {
+		const auto a(make_random_array());
+		ascension::detail::GapVector<int> gv(a.cbegin(), a.cend()), gv2;
+		std::swap(gv, gv2);
+		BOOST_TEST(gv.empty());
+		BOOST_CHECK_EQUAL_COLLECTIONS(gv2.cbegin(), gv2.cend(), a.cbegin(), a.cend());
+	}
+BOOST_AUTO_TEST_SUITE_END()
---
+++ ascension/test/src/numeric-range-test.cpp
@@ -0,0 +1,192 @@
+#define BOOST_TEST_MODULE numeric_range_test
+#include <boost/test/included/unit_test.hpp>
+
+#include <ascension/corelib/numeric-range.hpp>
+#include <ascension/corelib/numeric-range-algorithm/clamp.hpp>
+#include <ascension/corelib/numeric-range-algorithm/encompasses.hpp>
+#include <ascension/corelib/numeric-range-algorithm/hull.hpp>
+#include <ascension/corelib/numeric-range-algorithm/includes.hpp>
+#include <ascension/corelib/numeric-range-algorithm/intersection.hpp>
+#include <ascension/corelib/numeric-range-algorithm/order.hpp>
+#include <ascension/corelib/numeric-range-algorithm/overlaps.hpp>
+
+namespace {
+	struct Fixture {
+		const ascension::NumericRange<int> bounds;
+		ascension::NumericRange<int> ranges[9];
+
+		Fixture() : bounds(ascension::nrange(23, 42)) BOOST_NOEXCEPT {
+			ranges[0] = ascension::nrange(-9, 11);
+			ranges[1] = ascension::nrange(-9, 23);
+			ranges[2] = ascension::nrange(11, 31);
+			ranges[3] = ascension::nrange(23, 31);
+			ranges[4] = ascension::nrange(23, 42);
+			ranges[5] = ascension::nrange(31, 31);
+			ranges[6] = ascension::nrange(31, 42);
+			ranges[7] = ascension::nrange(31, 99);
+			ranges[8] = ascension::nrange(99, 99);
+		}
+	};
+}
+
+namespace boost {
+	std::ostream& operator<<(std::ostream& out, const optional<ascension::NumericRange<int>>& object) {
+		return out << "<" << ((object == boost::none) ? "none" : "not-none") << ">";
+	}
+}
+
+BOOST_AUTO_TEST_SUITE(construction)
+	BOOST_AUTO_TEST_CASE(default_constructor_test) {
+		ascension::NumericRange<int> nr;
+		BOOST_TEST(boost::empty(nr));
+
+		ascension::NumericRange<double> nr2;
+		BOOST_TEST(boost::empty(nr2));
+	}
+
+	BOOST_AUTO_TEST_CASE(constructor_with_values_test) {
+		ascension::NumericRange<int> nr(23, 42);
+		BOOST_TEST(*boost::begin(nr) == 23);
+		BOOST_TEST(*boost::end(nr) == 42);
+		BOOST_TEST(*boost::const_begin(nr) == 23);
+		BOOST_TEST(*boost::const_end(nr) == 42);
+
+		nr = ascension::NumericRange<int>(42, 23);
+		BOOST_TEST(*boost::const_begin(nr) == 42);
+		BOOST_TEST(*boost::const_end(nr) == 23);
+	}
+
+	BOOST_AUTO_TEST_CASE(copy_constructor_test) {
+		const ascension::NumericRange<int> nr(23, 42);
+		const auto nr2(nr);
+		BOOST_TEST(*boost::const_begin(nr) == *boost::const_begin(nr2));
+		BOOST_TEST(*boost::const_end(nr) == *boost::const_end(nr2));
+	}
+
+	BOOST_AUTO_TEST_CASE(nrange_test) {
+		auto nr(ascension::nrange(23, 42));
+	}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_FIXTURE_TEST_SUITE(algorithms, Fixture)
+	BOOST_AUTO_TEST_CASE(clamp_test) {
+		BOOST_TEST(ascension::clamp(-9, bounds) == 23);
+		BOOST_TEST(ascension::clamp(23, bounds) == 23);
+		BOOST_TEST(ascension::clamp(31, bounds) == 31);
+		BOOST_TEST(ascension::clamp(42, bounds) == 42);
+		BOOST_TEST(ascension::clamp(99, bounds) == 42);
+
+		// oops, BOOST_TEST(a == b) is ambiguous in these context...
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[0], bounds), ascension::nrange(23, 23));
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[1], bounds), ascension::nrange(23, 23));
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[2], bounds), ascension::nrange(23, 31));
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[3], bounds), ascension::nrange(23, 31));
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[4], bounds), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[5], bounds), ascension::nrange(31, 31));
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[6], bounds), ascension::nrange(31, 42));
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[7], bounds), ascension::nrange(31, 42));
+		BOOST_CHECK_EQUAL(ascension::clampRange(ranges[8], bounds), ascension::nrange(42, 42));
+
+		BOOST_CHECK_EQUAL((ranges[0] | ascension::adaptors::clamped(bounds)), ascension::nrange(23, 23));
+		BOOST_CHECK_EQUAL((ranges[1] | ascension::adaptors::clamped(bounds)), ascension::nrange(23, 23));
+		BOOST_CHECK_EQUAL((ranges[2] | ascension::adaptors::clamped(bounds)), ascension::nrange(23, 31));
+		BOOST_CHECK_EQUAL((ranges[3] | ascension::adaptors::clamped(bounds)), ascension::nrange(23, 31));
+		BOOST_CHECK_EQUAL((ranges[4] | ascension::adaptors::clamped(bounds)), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL((ranges[5] | ascension::adaptors::clamped(bounds)), ascension::nrange(31, 31));
+		BOOST_CHECK_EQUAL((ranges[6] | ascension::adaptors::clamped(bounds)), ascension::nrange(31, 42));
+		BOOST_CHECK_EQUAL((ranges[7] | ascension::adaptors::clamped(bounds)), ascension::nrange(31, 42));
+		BOOST_CHECK_EQUAL((ranges[8] | ascension::adaptors::clamped(bounds)), ascension::nrange(42, 42));
+	}
+
+	BOOST_AUTO_TEST_CASE(encompasses_test) {
+		BOOST_TEST(!ascension::encompasses(bounds, 11));
+		BOOST_TEST( ascension::encompasses(bounds, 23));
+		BOOST_TEST( ascension::encompasses(bounds, 31));
+		BOOST_TEST( ascension::encompasses(bounds, 42));
+		BOOST_TEST(!ascension::encompasses(bounds, 99));
+
+		BOOST_TEST(!ascension::encompasses(bounds, ranges[0]));
+		BOOST_TEST(!ascension::encompasses(bounds, ranges[1]));
+		BOOST_TEST(!ascension::encompasses(bounds, ranges[2]));
+		BOOST_TEST( ascension::encompasses(bounds, ranges[3]));
+		BOOST_TEST( ascension::encompasses(bounds, ranges[4]));
+		BOOST_TEST( ascension::encompasses(bounds, ranges[5]));
+		BOOST_TEST( ascension::encompasses(bounds, ranges[6]));
+		BOOST_TEST(!ascension::encompasses(bounds, ranges[7]));
+		BOOST_TEST(!ascension::encompasses(bounds, ranges[8]));
+	}
+
+	BOOST_AUTO_TEST_CASE(hull_test) {
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[0], bounds), ascension::nrange(-9, 42));
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[1], bounds), ascension::nrange(-9, 42));
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[2], bounds), ascension::nrange(11, 42));
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[3], bounds), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[4], bounds), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[5], bounds), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[6], bounds), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[7], bounds), ascension::nrange(23, 99));
+		BOOST_CHECK_EQUAL(ascension::hull(ranges[8], bounds), ascension::nrange(23, 99));
+	}
+
+	BOOST_AUTO_TEST_CASE(includes_test) {
+		BOOST_TEST(!ascension::includes(bounds, 11));
+		BOOST_TEST( ascension::includes(bounds, 23));
+		BOOST_TEST( ascension::includes(bounds, 31));
+		BOOST_TEST(!ascension::includes(bounds, 42));
+		BOOST_TEST(!ascension::includes(bounds, 99));
+	}
+
+	BOOST_AUTO_TEST_CASE(intersection_test) {
+		auto intersection(ascension::intersection(ranges[0], bounds));
+		BOOST_TEST(intersection == boost::none);
+		intersection = ascension::intersection(ranges[1], bounds);
+		BOOST_TEST(intersection != boost::none);
+		BOOST_CHECK_EQUAL(*intersection, ascension::nrange(23, 23));
+		intersection = ascension::intersection(ranges[2], bounds);
+		BOOST_TEST(intersection != boost::none);
+		BOOST_CHECK_EQUAL(*intersection, ascension::nrange(23, 31));
+		intersection = ascension::intersection(ranges[3], bounds);
+		BOOST_TEST(intersection != boost::none);
+		BOOST_CHECK_EQUAL(*intersection, ascension::nrange(23, 31));
+		intersection = ascension::intersection(ranges[4], bounds);
+		BOOST_TEST(intersection != boost::none);
+		BOOST_CHECK_EQUAL(*intersection, ascension::nrange(23, 42));
+		intersection = ascension::intersection(ranges[5], bounds);
+		BOOST_TEST(intersection != boost::none);
+		BOOST_CHECK_EQUAL(*intersection, ascension::nrange(31, 31));
+		intersection = ascension::intersection(ranges[6], bounds);
+		BOOST_TEST(intersection != boost::none);
+		BOOST_CHECK_EQUAL(*intersection, ascension::nrange(31, 42));
+		intersection = ascension::intersection(ranges[7], bounds);
+		BOOST_TEST(intersection != boost::none);
+		BOOST_CHECK_EQUAL(*intersection, ascension::nrange(31, 42));
+		intersection = ascension::intersection(ranges[8], bounds);
+		BOOST_TEST(intersection == boost::none);
+	}
+
+	BOOST_AUTO_TEST_CASE(order_test) {
+		BOOST_TEST( ascension::isOrdered(ascension::nrange(23, 42)));
+		BOOST_TEST(!ascension::isOrdered(ascension::nrange(42, 23)));
+		BOOST_TEST( ascension::isOrdered(ascension::nrange(31, 31)));
+
+		BOOST_CHECK_EQUAL(ascension::order(ascension::nrange(23, 42)), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::order(ascension::nrange(42, 23)), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::order(ascension::nrange(31, 31)), ascension::nrange(31, 31));
+
+		BOOST_CHECK_EQUAL(ascension::nrange(23, 42) | ascension::adaptors::ordered(), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::nrange(42, 23) | ascension::adaptors::ordered(), ascension::nrange(23, 42));
+		BOOST_CHECK_EQUAL(ascension::nrange(31, 31) | ascension::adaptors::ordered(), ascension::nrange(31, 31));
+	}
+
+	BOOST_AUTO_TEST_CASE(overlaps_test) {
+		BOOST_TEST(!ascension::overlaps(ranges[0], bounds));
+		BOOST_TEST(!ascension::overlaps(ranges[1], bounds));
+		BOOST_TEST( ascension::overlaps(ranges[2], bounds));
+		BOOST_TEST( ascension::overlaps(ranges[3], bounds));
+		BOOST_TEST( ascension::overlaps(ranges[4], bounds));
+		BOOST_TEST( ascension::overlaps(ranges[5], bounds));
+		BOOST_TEST( ascension::overlaps(ranges[6], bounds));
+		BOOST_TEST( ascension::overlaps(ranges[7], bounds));
+		BOOST_TEST(!ascension::overlaps(ranges[8], bounds));
+	}
+BOOST_AUTO_TEST_SUITE_END()
