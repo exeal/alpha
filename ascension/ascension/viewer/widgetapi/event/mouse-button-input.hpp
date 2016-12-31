3b81a26531a02a4e68f380f492f8faa058f3c348
exeal <luigi.irmr@gmail.com>
2017-01-01 00:45:22 +0900

    fixed: Many compile errors.

--- ascension/ascension/viewer/widgetapi/event/mouse-button-input.hpp
+++ ascension/ascension/viewer/widgetapi/event/mouse-button-input.hpp
@@ -37,6 +37,41 @@ namespace ascension {
 			}
 		}
 	}
+
+#if ASCENSION_SUPPORTS_WINDOW_SYSTEM(WIN32)
+	namespace win32 {
+		inline viewer::widgetapi::event::MouseButtonInput makeMouseButtonInput(viewer::widgetapi::event::LocatedUserInput::MouseButton button, WPARAM wp, LPARAM lp) {
+			return viewer::widgetapi::event::MouseButtonInput(makeMouseLocation<graphics::Point>(lp), button, makeModifiers(wp));
+		}
+
+		template<typename Point>
+		inline viewer::widgetapi::event::MouseButtonInput makeMouseButtonInput(DWORD keyState, const Point& location) {
+			viewer::widgetapi::UserInput::MouseButton buttons = 0;
+			if(boole(keyState & MK_LBUTTON))
+				buttons |= viewer::widgetapi::UserInput::BUTTON1_DOWN;
+			if(boole(keyState & MK_MBUTTON))
+				buttons |= viewer::widgetapi::UserInput::BUTTON2_DOWN;
+			if(boole(keyState & MK_RBUTTON))
+				buttons |= viewer::widgetapi::UserInput::BUTTON3_DOWN;
+			if(boole(keyState & MK_XBUTTON1))
+				buttons |= viewer::widgetapi::UserInput::BUTTON4_DOWN;
+			if(boole(keyState & MK_XBUTTON2))
+				buttons |= viewer::widgetapi::UserInput::BUTTON5_DOWN;
+			viewer::widgetapi::UserInput::ModifierKey modifiers = 0;
+			if(boole(keyState & MK_CONTROL))
+				modifiers |= viewer::widgetapi::UserInput::CONTROL_DOWN;
+			if(boole(keyState & MK_SHIFT))
+				modifiers |= viewer::widgetapi::UserInput::SHIFT_DOWN;
+			if(boole(keyState & MK_ALT))
+				modifiers |= viewer::widgetapi::UserInput::ALT_DOWN;
+			return viewer::widgetapi::MouseButtonInput(
+				boost::geometry::make<graphics::Point>(
+					static_cast<typename boost::geometry::coordinate_type<graphics::Point>::type>(geometry::x(location)),
+					static_cast<typename boost::geometry::coordinate_type<graphics::Point>::type>(geometry::y(location))),
+				buttons, modifiers);
+		}
+	}
+#endif // ASCENSION_SUPPORTS_WINDOW_SYSTEM(WIN32)
 }
 
 #endif // !ASCENSION_MOUSE_BUTTON_INPUT_HPP
