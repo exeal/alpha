fbeb92012e41ab7dd5d2dcb0ab579d482234978b
exeal <exeal@users.sourceforge.jp>
2017-01-01 23:27:40 +0900

    Added ascension.Combination class template. KeyboardModifiers now derives from Combination<KeyboardModifier>.

---
+++ ascension/ascension/corelib/combination.hpp
@@ -0,0 +1,70 @@
+/**
+ * @file combination.hpp
+ * Defines @c Combination class template.
+ * @author exeal
+ * @date 2017-01-01 Separated from keyboard-modifier.hpp.
+ */
+
+#ifndef ASCENSION_COMBINATION_HPP
+#define ASCENSION_COMBINATION_HPP
+#include <bitset>
+#include <type_traits>
+
+namespace ascension {
+	template<typename T, std::size_t N>
+	class Combination : public std::bitset<N> {
+		static_assert(std::is_enum<T>::value, "");
+	public:
+		typedef T value_type;
+
+		/// Creates an empty @c Combination object.
+		BOOST_CONSTEXPR Combination() BOOST_NOEXCEPT {}
+
+		/**
+		 * Creates a @c Combination object which has only the specified singular value.
+		 * @param bit The bit
+		 * @throw std#out_of_range @a value is invalid
+		 */
+		Combination(value_type bit) {
+			set(bit);
+		}
+
+		/**
+	 	 * Creates a @c Combination object with the specified bits.
+		 * @tparam Sequence The fixed-size sequence of @c value_type
+		 * @param sequence The sequence of @c value_type bits
+		 * @throw std#out_of_range @a sequence has invalid value
+		 */
+		template<typename Sequence>
+		explicit Combination(const Sequence& sequence) {
+			_set(sequence, std::integral_constant<std::size_t, std::tuple_size<Sequence>::value - 1>());
+		}
+
+		using std::bitset<N>::operator==;
+		using std::bitset<N>::operator!=;
+		using std::bitset<N>::operator&=;
+		using std::bitset<N>::operator|=;
+		using std::bitset<N>::operator^=;
+
+	private:
+		bool operator==(T) const;
+		bool operator!=(T) const;
+		Combination& operator&=(T);
+		Combination& operator|=(T);
+		Combination& operator^=(T);
+		Combination operator&(T) const;
+		Combination operator|(T) const;
+		Combination operator^(T) const;
+		template<typename Sequence, std::size_t M>
+		void _set(const Sequence& sequence, std::integral_constant<std::size_t, M>, typename std::enable_if<M != 0>::type* = nullptr) {
+			set(std::get<M>(sequence));
+			return _set(sequence, std::integral_constant<std::size_t, M - 1>());
+		}
+		template<typename Sequence>
+		void _set(const Sequence& sequence, std::integral_constant<std::size_t, 0>) {
+			set(std::get<0>(sequence));
+		}
+	};
+}
+
+#endif // !ASCENSION_COMBINATION_HPP
--- ascension/ascension/viewer/widgetapi/event/keyboard-modifier.hpp
+++ ascension/ascension/viewer/widgetapi/event/keyboard-modifier.hpp
@@ -8,6 +8,7 @@
 
 #ifndef ASCENSION_KEYBOARD_MODIFIER_HPP
 #define ASCENSION_KEYBOARD_MODIFIER_HPP
+#include <ascension/corelib/combination.hpp>
 #if ASCENSION_SELECTS_WINDOW_SYSTEM(GTK)
 #	include <gdkmm/types.h>
 #elif ASCENSION_SELECTS_WINDOW_SYSTEM(QUARTZ)
@@ -36,120 +37,67 @@ namespace ascension {
 				};
 
 				/// Indicates the state of modifier keys.
-				class KeyboardModifiers : public std::bitset<NUMBER_OF_KEYBOARD_MODIFIERS> {
-				public:
-					typedef
-#if ASCENSION_SELECTS_WINDOW_SYSTEM(GTK)
-						Gdk::ModifierType
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(QUARTZ)
-						// TODO: Specify native type.
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(QT)
-						Qt::KeyboardModifiers
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(WIN32)
-						WORD
-#endif
-						Native;	///< Native type.
+				typedef Combination<KeyboardModifier, NUMBER_OF_KEYBOARD_MODIFIERS> KeyboardModifiers;
 
-					/// Default constructor creates an empty @c KeyboardModifiers object.
-					KeyboardModifiers() BOOST_NOEXCEPT {
-					}
-					/**
-					 * Creates a @c KeyboardModifiers which has only the specified singular value.
-					 * @param modifier The modifier bit
-					 * @throw std#out_of_range @a modifier is invalid
-					 */
-					KeyboardModifiers(KeyboardModifier modifier) {
-						set(modifier);
-					}
-					/**
-					 * Creates a @c KeyboardModifiers object with the specified @c KeyboardModifier bits.
-					 * @tparam Sequence The fixed-size sequence of @c KeyboardModifier
-					 * @param sequence The sequence of @c KeyboardModifier bits
-					 * @throw std#out_of_range @a sequence has invalid value
-					 */
-					template<typename Sequence>
-					explicit KeyboardModifiers(const Sequence& sequence) {
-						_set(sequence, std::integral_constant<std::size_t, std::tuple_size<Sequence>::value - 1>());
-					}
-					/**
-					 * Creates a @c KeyboardModifiers object from the specified platform-native value.
-					 * @param source The native value
-					 */
-					static KeyboardModifiers fromNative(const Native& source) BOOST_NOEXCEPT;
-					/// Returns the platform-native value.
-					Native native() const BOOST_NOEXCEPT;
-
-					using std::bitset<NUMBER_OF_KEYBOARD_MODIFIERS>::operator==;
-					using std::bitset<NUMBER_OF_KEYBOARD_MODIFIERS>::operator!=;
-					using std::bitset<NUMBER_OF_KEYBOARD_MODIFIERS>::operator&=;
-					using std::bitset<NUMBER_OF_KEYBOARD_MODIFIERS>::operator|=;
-					using std::bitset<NUMBER_OF_KEYBOARD_MODIFIERS>::operator^=;
-
-				private:
-					bool operator==(KeyboardModifier) const;
-					bool operator!=(KeyboardModifier) const;
-					KeyboardModifiers& operator&=(KeyboardModifier);
-					KeyboardModifiers& operator|=(KeyboardModifier);
-					KeyboardModifiers& operator^=(KeyboardModifier);
-					KeyboardModifiers operator&(KeyboardModifier) const;
-					KeyboardModifiers operator|(KeyboardModifier) const;
-					KeyboardModifiers operator^(KeyboardModifier) const;
-					template<typename Sequence, std::size_t N>
-					void _set(const Sequence& sequence, std::integral_constant<std::size_t, N>, typename std::enable_if<N != 0>::type* = nullptr) {
-						set(std::get<N>(sequence));
-						return _set(sequence, std::integral_constant<std::size_t, N - 1>());
-					}
-					template<typename Sequence>
-					void _set(const Sequence& sequence, std::integral_constant<std::size_t, 0>) {
-						set(std::get<0>(sequence));
-					}
-				};
-
-				inline KeyboardModifiers KeyboardModifiers::fromNative(const Native& source) BOOST_NOEXCEPT {
+#if ASCENSION_SUPPORTS_WINDOW_SYSTEM(GTK)
+				template<typename Model>
+				inline Model fromNative(const Gdk::ModifierType& source, typename std::enable_if<std::is_same<Model, KeyboardModifiers>::value>::type* = nullptr) BOOST_NOEXCEPT {
 					KeyboardModifiers value;
-#if ASCENSION_SELECTS_WINDOW_SYSTEM(GTK)
 					value.set(SHIFT_DOWN, (source & Gdk::SHIFT_MASK) != 0);
 					value.set(CONTROL_DOWN, (source & Gdk::CONTROL_MASK) != 0);
 					value.set(ALT_DOWN, (source & Gdk::MOD1_MASK) != 0);
 					value.set(META_DOWN, (source & Gdk::META_MASK) != 0);
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(QUARTZ)
+				}
+
+				inline Gdk::ModifierType toNative(const KeyboardModifiers& from, const Gdk::ModifierType* = nullptr) BOOST_NOEXCEPT {
+					Gdk::ModifierType value = static_cast<Gdk::ModifierType>(0);
+					value |= from.test(SHIFT_DOWN) ? Gdk::SHIFT_MASK : static_cast<Gdk::ModifierType>(0);
+					value |= from.test(CONTROL_DOWN) ? Gdk::CONTROL_MASK : static_cast<Gdk::ModifierType>(0);
+					value |= from.test(ALT_DOWN) ? Gdk::MOD1_MASK : static_cast<Gdk::ModifierType>(0);
+					value |= from.test(META_DOWN) ? Gdk::META_MASK : static_cast<Gdk::ModifierType>(0);
+					return value;
+				}
+#endif
+#if ASCENSION_SUPPORTS_WINDOW_SYSTEM(QUARTZ)
 #	error Not implemented.
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(QT)
+#endif
+#if ASCENSION_SUPPORTS_WINDOW_SYSTEM(QT)
+				template<typename Model>
+				inline Model fromNative(const Qt::KeyboardModifiers& source, typename std::enable_if<std::is_same<Model, KeyboardModifiers>::value>::type* = nullptr) BOOST_NOEXCEPT {
+					KeyboardModifiers value;
 					value.set(SHIFT_DOWN, source.testFlag(Qt::ShiftModifier));
 					value.set(CONTROL_DOWN, source.testFlag(Qt::ControlModifier));
 					value.set(ALT_DOWN, source.testFlag(Qt::AltModifier));
 					value.set(META_DOWN, source.testFlag(Qt::MetaModifier));
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(WIN32)
-					value.set(SHIFT_DOWN, (source & MOD_SHIFT) != 0);
-					value.set(CONTROL_DOWN, (source & MOD_CONTROL) != 0);
-					value.set(ALT_DOWN, (source & MOD_ALT) != 0);
-					value.set(META_DOWN, (source & MOD_WIN) != 0);
+				}
+
+				inline Qt::KeyboardModifiers toNative(const KeyboardModifiers& from, const Qt::KeyboardModifiers* = nullptr) BOOST_NOEXCEPT {
+					Qt::KeyboardModifiers value = Qt::NoModifier;
+					value |= from.test(SHIFT_DOWN) ? Qt::ShiftModifier : Qt::NoModifier;
+					value |= from.test(CONTROL_DOWN) ? Qt::ControlModifier : Qt::NoModifier;
+					value |= from.test(ALT_DOWN) ? Qt::AltModifier : Qt::NoModifier;
+					value |= from.test(META_DOWN) ? Qt::MetaModifier : Qt::NoModifier;
+					return value;
+				}
 #endif
+#if ASCENSION_SUPPORTS_WINDOW_SYSTEM(WIN32)
+				template<typename Model>
+				inline Model fromNative(WORD source, typename std::enable_if<std::is_same<Model, KeyboardModifiers>::value>::type* = nullptr) BOOST_NOEXCEPT {
+					KeyboardModifiers value;
+					value.set(SHIFT_DOWN, (source & MK_SHIFT) != 0);
+					value.set(CONTROL_DOWN, (source & MK_CONTROL) != 0);
+					value.set(META_DOWN, (source & MK_ALT) != 0);	// for D&D
 					return value;
 				}
 
-				inline KeyboardModifiers::Native KeyboardModifiers::native() const BOOST_NOEXCEPT {
-					Native value = static_cast<Native>(0);
-#if ASCENSION_SELECTS_WINDOW_SYSTEM(GTK)
-					value |= test(SHIFT_DOWN) ? Gdk::SHIFT_MASK : Native(0);
-					value |= test(CONTROL_DOWN) ? Gdk::CONTROL_MASK : Native(0);
-					value |= test(ALT_DOWN) ? Gdk::MOD1_MASK : Native(0);
-					value |= test(META_DOWN) ? Gdk::META_MASK : Native(0);
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(QUARTZ)
-#	error Not implemented.
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(QT)
-					value |= test(SHIFT_DOWN) ? Qt::ShiftModifier : Qt::NoModifier;
-					value |= test(CONTROL_DOWN) ? Qt::ControlModifier : Qt::NoModifier;
-					value |= test(ALT_DOWN) ? Qt::AltModifier : Qt::NoModifier;
-					value |= test(META_DOWN) ? Qt::MetaModifier : Qt::NoModifier;
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(WIN32)
-					value |= test(SHIFT_DOWN) ? MOD_SHIFT : 0;
-					value |= test(CONTROL_DOWN) ? MOD_CONTROL : 0;
-					value |= test(ALT_DOWN) ? MOD_ALT : 0;
-					value |= test(META_DOWN) ? MOD_WIN : 0;
-#endif
+				inline WORD toNative(const KeyboardModifiers& from, const WORD* = nullptr) BOOST_NOEXCEPT {
+					WORD value = 0;
+					value |= from.test(SHIFT_DOWN) ? MK_SHIFT : 0;
+					value |= from.test(CONTROL_DOWN) ? MK_CONTROL : 0;
+					value |= from.test(META_DOWN) ? MK_ALT : 0;	// for D&D
 					return value;
 				}
+#endif
 			}
 		}
 	}
