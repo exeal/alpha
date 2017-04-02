b1b13ed7c2999597c4b1c6b17246e807c5b1c37a
exeal <luigi.irmr@gmail.com>
2017-04-02 22:56:45 +0900

    Implemented iterator and const_iterator classes of EditorPanes.

--- alpha/src/editor-panes.hpp
+++ alpha/src/editor-panes.hpp
@@ -11,7 +11,6 @@
 //#include "editor-pane.hpp"
 #include <ascension/corelib/signals.hpp>
 #include <boost/iterator/iterator_facade.hpp>
-#include <stack>
 #if ASCENSION_SELECTS_WINDOW_SYSTEM(GTK)
 #	include <gtkmm/paned.h>
 #elif ASCENSION_SELECTS_WINDOW_SYSTEM(QT)
@@ -41,66 +40,77 @@ namespace alpha {
 		friend class EditorView;
 	};
 
-	class EditorPanes : public FocusChain,
+	namespace detail {
+		class Paned :
 #if ASCENSION_SELECTS_WINDOW_SYSTEM(GTK)
-		public Gtk::Paned, private boost::noncopyable
+			public Gtk::Paned, private boost::noncopyable
 #elif ASCENSION_SELECTS_WINDOW_SYSTEM(QT)
-		public QSplitter, private boost::noncopyable
+			public QSplitter, private boost::noncopyable
 #elif ASCENSION_SELECTS_WINDOW_SYSTEM(QUARTZ)
-		???
+			???
 #elif ASCENSION_SELECTS_WINDOW_SYSTEM(WIN32)
-		public win32::PanedWidget
+			public win32::PanedWidget
 #endif
-		{	// children may be either native widget or EditorPane
-	private:
-		template<typename Derived, typename Value>
-		class InternalIterator : public boost::iterators::iterator_facade<Derived, Value, boost::iterators::bidirectional_traversal_tag> {
+		{
 		public:
-			InternalIterator(typename iterator_facade_::pointer pane);
+			Paned* parent() const BOOST_NOEXCEPT {
+				return parent_;
+			}
+		protected:
+#if ASCENSION_SELECTS_WINDOW_SYSTEM(QT)
+			explicit Paned(Paned* parentPaned, QWidget* parent = Q_NULLPTR);
+#else
+			explicit Paned(Paned* parentPaned);
+#endif
 		private:
+			Paned* parent_;
+		};
+	}
+
+	class EditorPanes : public FocusChain, public detail::Paned {	// children may be either Paned or EditorPane
+	private:
+		template<typename Value>
+		class InternalIterator : public boost::iterators::iterator_facade<InternalIterator<Value>, Value, boost::iterators::bidirectional_traversal_tag> {
+		public:
+			typedef detail::Paned MutablePaned;
+			typedef typename std::conditional<std::is_const<Value>::value, const MutablePaned, MutablePaned>::type Paned;
+			static InternalIterator makeFirst(Paned& root);
+			static InternalIterator makeLast(Paned& root);
+		private:
+			template<std::size_t branch> static InternalIterator make(Paned& root);
+			// boost.iterators.iterator_facade
 			friend class boost::iterators::iterator_core_access;
 			void decrement();
 			typename iterator_facade_::reference dereference() const;
 			bool equal(const InternalIterator& other) const;
 			void increment();
 		private:
-			typename iterator_facade_::pointer current_;
-			bool end_;
-#if ASCENSION_SELECTS_WINDOW_SYSTEM(QT)
-			std::shared_ptr<std::stack<QSplitter*>> ancestors_;
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(QUARTZ)
-			std::shared_ptr<std::stack<???*>> ancestors_;
-#elif ASCENSION_SELECTS_WINDOW_SYSTEM(WIN32)
-			std::shared_ptr<std::stack<win32::PanedWidget*>> ancestors_;
-#endif
+			std::shared_ptr<Value> current_;
+			detail::Paned* parent_;
 		};
 
 	public:
-		class Iterator : public InternalIterator<Iterator, EditorPane> {
-		public:
-			Iterator(pointer pane) : InternalIterator<Iterator, EditorPane>(pane) {}
-		};
-		class ConstIterator : public InternalIterator<ConstIterator, const EditorPane> {
-		public:
-			ConstIterator(pointer pane) : InternalIterator<ConstIterator, const EditorPane>(pane) {}
-		};
-		typedef Iterator iterator;
-		typedef ConstIterator const_iterator;
+		typedef InternalIterator<EditorPane> iterator;
+		typedef InternalIterator<const EditorPane> const_iterator;
 		typedef std::ptrdiff_t difference_type;
 
 	public:
+#if ASCENSION_SELECTS_WINDOW_SYSTEM(QT)
+		EditorPanes(QWidget* parent = Q_NULLPTR);
+#else
 		EditorPanes();
+#endif
 
 		/// @name Pane Access
 		/// @{
 		EditorPane& activePane() BOOST_NOEXCEPT;
 		const EditorPane& activePane() const BOOST_NOEXCEPT;
-		Iterator begin() BOOST_NOEXCEPT;
-		ConstIterator begin() const BOOST_NOEXCEPT;
-		ConstIterator cbegin() const BOOST_NOEXCEPT;
-		ConstIterator cend() const BOOST_NOEXCEPT;
-		Iterator end() BOOST_NOEXCEPT;
-		ConstIterator end() const BOOST_NOEXCEPT;
+		iterator begin() BOOST_NOEXCEPT;
+		const_iterator begin() const BOOST_NOEXCEPT;
+		const_iterator cbegin() const BOOST_NOEXCEPT;
+		const_iterator cend() const BOOST_NOEXCEPT;
+		iterator end() BOOST_NOEXCEPT;
+		const_iterator end() const BOOST_NOEXCEPT;
 		/// @}
 
 		/// @name Paned Operations
